3de8b7e29a33d5b1894d59bbcc608d8c3eb02589
sahar <s.shafiee_73@yahoo.com>
2021-07-10 18:24:42 +0430

    add mobile image

--- components/mobileHeader/index.jsx
+++ components/mobileHeader/index.jsx
@@ -1,10 +1,6 @@
 // node libraries
 import Image from 'next/image';
 import { useRouter } from 'next/router';
-// image
-// import search from '../../public/image/headerIcon/search.svg';
-// import close from '../../public/image/headerIcon/close.svg';
-// import bascket from '../../public/image/headerIcon/bascket.svg';
 // scss
 import styles from '../../styles/components/mobileHeader/mobileHeader.module.scss';
 /**
@@ -21,11 +17,11 @@ function MobileHeader({
         <div className={type === "back" ? `${styles.wrapper_header_back}` : `${styles.wrapper_header}`}
             onClick={() => router.back()}
         >
-            {type === "back" && <Image src="/back.svg" alt="header icon" />}
+            {type === "back" && <Image src="/image/mobileHeader/back.svg" alt="header icon" />}
             <span className={styles.header_span}>{title}</span>
-            {/* {type === "search" && <Image src={search} alt="header icon" />}
-            {type === "close" && <Image src={close} alt="header icon" />}
-            {type === "bascket" && <Image src={bascket} alt="header icon" />} */}
+            {type === "search" && <Image src="/image/mobileHeader/search.svg" alt="header icon" />}
+            {type === "close" && <Image src="/image/mobileHeader/close.svg" alt="header icon" />}
+            {type === "bascket" && <Image src="/image/mobileHeader/bascket.svg" alt="header icon" />}
         </div>
     );
 }
