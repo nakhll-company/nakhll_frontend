99eeed51535e73191785012907d9647f8a6c1e5e
sahar <s.shafiee_73@yahoo.com>
2021-07-12 17:57:27 +0430

    get edit price data

--- pages/fp/product/update/price/index.jsx
+++ pages/fp/product/update/price/index.jsx
@@ -1,10 +1,14 @@
+// node libraries
+import { connect } from 'react-redux';
 // component
 import Layout from '../../../../../components/layout/Layout';
 import MobileHeader from '../../../../../components/mobileHeader';
+// methods
+import { mapState } from '../../../../../containers/product/methods/mapState';
 // scss
 import styles from '../../../../../styles/pages/product/editPrice.module.scss';
 
-const Price = () => {
+const Price = ({ productList }) => {
     return (
         <div className={styles.wrapper}>
             <MobileHeader title="ویرایش قیمت و تخفیف محصولات" type="back" />
@@ -13,13 +17,19 @@ const Price = () => {
                 <span>قیمت</span>
                 <span>قیمت با تخفیف</span>
             </div>
-            <form className={styles.form_edit}>
-                {[1, 2, 3].map((value, index) => {
+            <form className={styles.form_edit} onSubmit={(e) => {
+                e.preventDefault();
+                const data = new FormData(e.target);
+                console.log(Object.fromEntries(data.entries()));
+            }}>
+                {productList.map((value, index) => {
                     return (
                         <div key={index} className={styles.form_edit_card}>
-                            <label>گل محمدی</label>
-                            <input className={styles.form_edit_input} type="number" name="price" id="" />
-                            <input className={styles.form_edit_input} type="number" name="price" id="" />
+                            <label className={styles.form_edit_label}>{value.title}</label>
+                            <input className={styles.form_edit_input} type="number" name={`old_price${index}`}
+                                defaultValue={value.old_price} />
+                            <input className={styles.form_edit_input} type="number" name={`price${index}`}
+                                defaultValue={value.price} />
                         </div>
                     )
                 })}
@@ -31,5 +41,6 @@ const Price = () => {
     );
 }
 // export
-export default Price;
+const connector = connect(mapState);
+export default connector(Price);
 Price.Layout = Layout;
