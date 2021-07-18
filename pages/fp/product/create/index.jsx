bf28eaf4aacec8e2582e92c909c93bb33e3ec47e
miillaadddddd <milad7212@gmail.com>
2021-07-18 15:36:12 +0430

    initial createProduct

---
+++ pages/fp/product/create/index.jsx
@@ -0,0 +1,13 @@
+import MyLayout from "../../../../components/layout/Layout";
+
+function HomePage() {
+  return (
+    <div>
+      <h1>millllllllaaaaddddddddddddd</h1>
+    </div>
+  );
+}
+
+export default HomePage;
+
+HomePage.Layout = MyLayout;
