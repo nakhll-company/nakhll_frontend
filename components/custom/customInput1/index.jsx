b7889d3b9a42e09ebb093ec5097495b5fc685b3e
miillaadddddd <milad7212@gmail.com>
2021-07-19 08:17:42 +0430

    creat product

--- components/custom/customInput1/index.jsx
+++ components/custom/customInput1/index.jsx
@@ -1,12 +1,13 @@
 import styles from "./customInput.module.scss";
+import {Field} from "formik"
 
-const CustomInput1 = ({ label, explain }) => {
+const CustomInput1 = ({ label, explain,name,...otherProps }) => {
   return (
     <div className={styles.twoCol}>
       <div>
         <h2 style={{ marginBottom: "10px", color: "#364254" }}>{label}</h2>
         <div className={styles.inputWidRtl}>
-          <input type="text" />
+          <Field type="input" />
         </div>
       </div>
       <div>
--- pages/fp/product/create/index.jsx
+++ pages/fp/product/create/index.jsx
@@ -8,10 +8,82 @@ import CustomInput2 from "../../../../components/custom/customInput2/index";
 import CustomInputArea from "../../../../components/custom/customInputArea/index";
 import CustomInputWithBtn from "../../../../components/custom/customInputwithBtn/index";
 
+// formik
+import { Formik, Form } from "formik";
+import * as Yup from "yup";
 function HomePage() {
+  const INITIAL_FORM_STATE = {
+    firstName: "",
+    lastName: "",
+    sex: "",
+    code: "",
+    email: "",
+    phone: "",
+    addressLine1: "",
+    addressLine2: "",
+    citey: "",
+    state: "",
+    countery: "",
+    arrivelDate: "",
+    deputureDate: "",
+    message: "",
+    termsOfService: false,
+    codePosti: "",
+    telephone: "",
+    Me: "",
+  };
+
+  const FORM_VALIDAITIOM = Yup.object().shape({
+    firstName: Yup.string().required("الزامی"),
+    lastName: Yup.string().required("الزامی"),
+    email: Yup.string().email("وجود ندارد").required("الزامی"),
+    phone: Yup.number()
+      .integer()
+      .typeError("مقدار  نامجرا است")
+      .required("الزامی"),
+    addressLine1: Yup.string().required("الزامی"),
+    addressLine2: Yup.string(),
+    citey: Yup.string().required("الزامی"),
+    state: Yup.string().required("الزامی"),
+    countery: Yup.string().required("الزامی"),
+    arrivelDate: Yup.date().required("الزامی"),
+    deputureDate: Yup.date().required("الزامی"),
+    message: Yup.string(),
+    termsOfService: Yup.boolean()
+      .oneOf([true], "Ghabool Kardi???")
+      .required("hatman bayad bezani"),
+    code: Yup.number()
+      .integer()
+      .typeError("مقدار نامجزا است")
+      .required("الزامی"),
+    sex: Yup.string().required("الزامی"),
+    codePosti: Yup.number()
+      .integer()
+      .typeError("مقدار نامجزا است")
+      .required("الزامی"),
+    telephone: Yup.number()
+      .integer()
+      .typeError("type errorr dadai")
+      .required("الزامی"),
+    Me: Yup.string(),
+  });
   return (
     <>
       <div className={styles.wrapper}>
+        <Formik
+          initialValues={{ ...INITIAL_FORM_STATE }}
+          validationSchema={FORM_VALIDAITIOM}
+          onSubmit={(values) => {
+            console.log(values);
+          }}
+        >
+            <Form>
+
+
+
+                
+            </Form>
+        </Formik>
         {/* نام محصول */}
         <CustomInputWithBtn label="نام محصول" explain="نام محصول را وارد کن" />
         <CustomInput1 label="نام محصول" explain="نام محصول را وارد کن" />
