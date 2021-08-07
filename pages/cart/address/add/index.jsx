66c2805ad40990a378069f89831d1fa748b04efb
sahar <s.shafiee_73@yahoo.com>
2021-08-07 13:26:51 +0430

    Add address details

---
+++ pages/cart/address/add/index.jsx
@@ -0,0 +1,10 @@
+/**
+ * add address
+ */
+const AddAddress = () => {
+    return (
+        <div>fffff</div>
+    )
+}
+// export
+export default AddAddress;
--- pages/cart/address/index.jsx
+++ pages/cart/address/index.jsx
@@ -1,12 +1,14 @@
 // node libraries
 import Head from "next/head";
 import Link from 'next/link';
+import Assistent from "zaravand-assistent-number";
 // styles
 import styles from '../../../styles/pages/cart/address.module.scss';
 /**
  * component address cart
  * @param {}  => 
  */
+const _asist = new Assistent();
 const Address = () => {
     return (
         <>
@@ -15,13 +17,10 @@ const Address = () => {
                     rel="stylesheet"
                     href="https://use.fontawesome.com/releases/v5.15.3/css/all.css"
                     integrity="sha384-SZXxX4whJ79/gErwcOYf+zWLeJdY/qpuqC4cAa9rOGUstPomtqpuNWT9wdPEn2fk"
-                    crossorigin="anonymous"
+                    crossOrigin="anonymous"
                 ></link>
             </Head>
             <div className={`col-12 col-lg-5 ${styles.wrapper}`}>
-                <div className="">
-                    <h1>milad</h1>
-                </div>
                 <header className={styles.header}>
                     <Link href="/cart">
                         <a className={styles.header_back_link}>
@@ -36,14 +35,23 @@ const Address = () => {
                         <span>می خواهید سفارش شما به کدام نشانی ارسال شود :</span>
                         <Link href="/cart/address/new">
                             <a className={styles.address_head_link}>
-                                <i class="fas fa-plus px-2"></i>
+                                <i className="fas fa-plus px-2"></i>
                                 یک نشانی جدید اضافه کنید
                             </a>
                         </Link>
                     </div>
-                    <form>
-                        <label>
-
+                    <form className={styles.address_items_form}>
+                        <label htmlFor="addressId" className={styles.address_items_label}>
+                            <div className={styles.address_item_circle}></div>
+                            <div className={styles.address_item_detail}>
+                                <b className="px-3">پسته خندون</b> <span>موبایل:</span> <b>{_asist.number('09137552590')}</b><br />
+                                <b className="px-3">زرند</b><span> / {_asist.number('خیابان فردوس کوچه 1 پلاک 87')}</span>
+                            </div>
+                            <div className={styles.address_item_icons}>
+                                <i class="far fa-edit 3x mx-3"></i>
+                                <i class="far fa-trash-alt"></i>
+                            </div>
+                            <input id="addressId" type="radio" name="addressId" value="1" />
                         </label>
                     </form>
                 </section>
