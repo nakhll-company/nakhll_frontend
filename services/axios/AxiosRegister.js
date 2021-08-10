57ff1ab14e22d9931efa46344ecad9b52b1a6967
SeyedHassan-mahdavi <mahdaviali19@yahoo.com>
2021-08-10 16:29:04 +0430

    edit axios baseUrl

--- services/axios/AxiosRegister.js
+++ services/axios/AxiosRegister.js
@@ -3,7 +3,7 @@ import Axios from "axios";
 //=================================================================\\
 export const instanceAxiosWithOutToken = Axios.create({
     // withCredentials: true,
-    baseURL: "http://localhost:8000/",
+    baseURL: "https://nakhll.com/",
     headers: {
         "Content-Type": " application/json",
     },
@@ -16,7 +16,7 @@ export const instanceAxiosWithOutToken = Axios.create({
 //=================================================================\\
 export const instanceAxiosWithToken = Axios.create({
     withCredentials: true,
-    baseURL: "http://localhost:8000/",
+    baseURL: "https://nakhll.com/",
     timeout: 300000,
     headers: {
         "Content-Type": " application/json",
