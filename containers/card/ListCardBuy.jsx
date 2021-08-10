cfb6880b5c1a04bc7cf3a2c6919d5b5d2a2d503d
miillaadddddd <milad7212@gmail.com>
2021-08-10 16:07:31 +0430

    Set  api in first peyment page

--- containers/card/ListCardBuy.jsx
+++ containers/card/ListCardBuy.jsx
@@ -1,12 +1,160 @@
 import React from "react";
+import { useContext } from "react";
 import Assistent from "zaravand-assistent-number";
+import ContextProduct from "../../pages/cart/Context/context";
 
 const _asist = new Assistent();
 
 export default function ListCardBuy() {
+  // GET "All_product_list_buy" FROM PARENT COMPONENT
+  const {
+    All_product_list_buy,
+    handel_AddProductTOList,
+    handel_DeleteProductFromList,
+  } = useContext(ContextProduct);
+  console.log("inja :>> ", All_product_list_buy);
   return (
     <div className="col-12 col-lg-8 mb-3 my-md-3 my-lg-0 order-1 order-md-1 order-lg-0">
       <div className="cart-items mt-2">
+        {All_product_list_buy.ordered_items &&
+          All_product_list_buy.ordered_items.map((El) => (
+            <div className="cart-product-group bg-white">
+              <div className="pt-3 pb-1 px-3">
+                <span className="font-size1">از غرفه: </span>{" "}
+                <a
+                  href={El.product.shop.url}
+                  className="vendor-link font-size1 font-weight-bold link-body font-weight-normal txtcut"
+                >
+                  {El.product.shop.title}
+                </a>
+              </div>
+
+              {/*</mnbvcxz> IF CHANGE IN PRODUCT IN LIST */}
+
+              <div className="align-items-center nakhl-label d-flex justify-content-between mx-3 mt-3 p-2 rounded  border border-danger text-danger">
+                <div className="mb-0 pr-2 font-size-sm">
+                  <div style={{ fontSize: "15px", fontWeight: "400" }}>
+                    از محصول روان نویس (خودکار) یونیکورن (اسب تک شاخ)به اندازه
+                    کافی موجود نمی‌باشد
+                  </div>
+                  <div>قیمت محصول تغییر کرده است</div>
+                </div>{" "}
+                <span className="dd-flex align-items-center mr-2 pointer font-size-sm text-nowrap">
+                  حذف
+                  <i
+                    className="fas fa-times-circle"
+                    style={{ fontSize: "20px", marginRight: "5px" }}
+                  ></i>
+                </span>
+              </div>
+
+              {/*^^^^^^^^^^^ IF CHANGE IN PRODUCT IN LIST ^^^^^^^^^^^*/}
+
+              <div className="p-3 mt-2 cart-product-item">
+                <div className="spinner spinner--medium"></div>
+                <div className="d-flex flex-wrap justify-content-between">
+                  <div className="d-flex w-100">
+                    <a
+                      href="/ali_goharrizi/product/746256?from=cart&amp;component=cart"
+                      className="product-link"
+                    >
+                      <img
+                        src="https://statics.basalam.com/public/users/13no0/2011/lW23nVJv4TiBsDxfJEKruxOpDVze1bTQSAHtqjHg.jpeg_256X256X70.jpeg"
+                        className="cart-product-item-img rounded"
+                      />
+                    </a>
+                    <div className="d-flex flex-column justify-content-between mr-3 w-100 overflow-hidden">
+                      <div
+                        style={{
+                          display: "flex",
+                          justifyContent: "space-between",
+                        }}
+                      >
+                        <a
+                          href={El.product.url}
+                          className="product-link d-block font-size1 link-body font-weight-bold text-truncate"
+                        >
+                          {_asist.number(El.product.title)}
+                        </a>
+                        <i
+                          className="fas fa-times-circle"
+                          style={{ fontSize: "20px", marginRight: "5px" }}
+                          onClick={() =>
+                            handel_DeleteProductFromList(El.product.id)
+                          }
+                        ></i>
+                      </div>
+                      <div className="cart-product-item-remain-stock"></div>
+                      <div className="nakhl-label mr-auto small teaberry-light">
+                        {_asist.number(El.product.discount)}
+                        <span> %</span>
+                      </div>
+                      <div className="d-flex align-items-center">
+                        <div
+                          className="mt-2 d-flex align-items-center"
+                          style={{ witheSpace: "nowrap" }}
+                        >
+                          <div
+                            className="quantity-box input-group input-group-sm"
+                            style={{ width: "7rem", alignItems: "center" }}
+                          >
+                            <div className="input-group-prepend">
+                              <button className="btn  plus-minus-icon">
+                                <i
+                                  style={{
+                                    fontSize: "25px",
+                                    color: "#1b3e68 ",
+                                  }}
+                                  className="fas fa-plus-square"
+                                  onClick={() =>
+                                    handel_AddProductTOList(El.product.id)
+                                  }
+                                ></i>
+                              </button>
+                            </div>
+                            <input
+                              min="0"
+                              type="text"
+                              disabled="disabled"
+                              value={_asist.number(`1`)}
+                              className="bg-white border-0 font-size1-2 font-weight-bold form-control mt-1 px-1 text-center"
+                            />
+                            <div className="input-group-append">
+                              <button className="btn  plus-minus-icon">
+                                <i
+                                  style={{
+                                    fontSize: "25px",
+                                    color: "#91a6c1 ",
+                                  }}
+                                  className="fas fa-minus-square"
+                                ></i>
+                              </button>
+                            </div>
+                          </div>
+                          <span className="d-inline-block font-size-9 mr-3 pointer">
+                            {" "}
+                            حذف؟{" "}
+                          </span>
+                        </div>
+                        <div className="mr-auto">
+                          <span className="cart-product-item-primary-price">
+                            {_asist.PSeparator(El.product.old_price)}
+                          </span>{" "}
+                          <span className="font-weight-bold">
+                            {_asist.PSeparator(El.product.price)}
+                          </span>{" "}
+                          <span>تومان</span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="v-portal" style={{ display: "none" }}></div>
+              </div>
+            </div>
+          ))}
+
         <div className="cart-product-group bg-white">
           <div className="pt-3 pb-1 px-3">
             <span className="font-size1">از غرفه: </span>{" "}
@@ -79,7 +227,7 @@ export default function ListCardBuy() {
                         <div className="input-group-prepend">
                           <button className="btn  plus-minus-icon">
                             <i
-                              style={{ fontSize: "25px", color: "#00c09d " }}
+                              style={{ fontSize: "25px", color: "#1b3e68 " }}
                               className="fas fa-plus-square"
                             ></i>
                           </button>
@@ -123,7 +271,7 @@ export default function ListCardBuy() {
           </div>
         </div>
 
-        <div className="cart-product-group bg-white">
+        {/* <div className="cart-product-group bg-white">
           <div className="pt-3 pb-1 px-3">
             <span className="font-size1">از غرفه: </span>{" "}
             <a
@@ -278,7 +426,7 @@ export default function ListCardBuy() {
 
             <div className="v-portal" style={{ display: "none" }}></div>
           </div>
-        </div>
+        </div> */}
       </div>
     </div>
   );
--- pages/cart/index.jsx
+++ pages/cart/index.jsx
@@ -1,37 +1,133 @@
+// node
+import { useEffect, useState } from "react";
 import Head from "next/head";
 
+// component
 import ListCardBuy from "../../containers/card/ListCardBuy";
 import MiniCardBuy from "../../containers/card/MiniCardBuy";
 import SumBuy from "../../containers/card/SumBuy";
 import ShopLayout from "../../components/shopLayout";
 import CheckOutSteps from "../../components/CheckOutSteps/CheckOutSteps";
-export default function Cart() {
-  return (
-    <div className="all">
-      <Head>
-        <link
-          rel="stylesheet"
-          href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
-          crossorigin="anonymous"
-        />
-        <link
-          rel="stylesheet"
-          href="https://use.fontawesome.com/releases/v5.15.3/css/all.css"
-          integrity="sha384-SZXxX4whJ79/gErwcOYf+zWLeJdY/qpuqC4cAa9rOGUstPomtqpuNWT9wdPEn2fk"
-          crossorigin="anonymous"
-        ></link>
-      </Head>
-      <section className="container container--mob pb-5 ">
-        <CheckOutSteps step="1" />
-        <MiniCardBuy />
+// metods
+import { ApiRegister } from "../../services/apiRegister/ApiRegister";
+import ContextProduct from "./Context/context";
 
-        <div className="row mx-auto mt-4" style={{ maxWidth: "72rem" }}>
-          <ListCardBuy />
-          <SumBuy />
-        </div>
-      </section>
-    </div>
+export default function Cart() {
+  // STATE FOR SAVE PRODUCTS
+  const [All_product_list_buy, setAll_product_list_buy] = useState([]);
+
+  // CALL API  &  GET PRODUCT LIST  & SET DATA IN "All_product_list_buy"
+
+  useEffect(() => {
+    const _handleRequestApi = async () => {
+      let params = {};
+      let loadData = null;
+      let dataUrl = `/cart2/api/carts/my/`;
+      let response = await ApiRegister().apiRequest(
+        loadData,
+        "get",
+        dataUrl,
+        true,
+        params
+      );
+
+      if (response.status === 200) {
+        setAll_product_list_buy(await response.data);
+        console.log("response.data :>> ", response.data);
+      }
+
+      // setSelectState(await getStates());
+    };
+
+    _handleRequestApi();
+  }, []);
+  console.log("All_product_list_buy :>> ", All_product_list_buy);
+
+  //  FUNCTION FOR ADD PRODUCT TO LIST  WHEN CLICKED ON PLUS BUTTON
+  const handel_AddProductTOList = (id) => {
+    const _handleRequestApi = async () => {
+      let params = {};
+      let loadData = null;
+      let dataUrl = `/cart2/api/cart_items/${id}/add/`;
+      let response = await ApiRegister().apiRequest(
+        loadData,
+        "get",
+        dataUrl,
+        true,
+        params
+      );
+
+      if (response.status === 200) {
+        console.log("Hamehchiokkkk :>> ", " Hamehchiokkkk");
+      }
+    };
+
+    _handleRequestApi();
+  };
+
+  // FUNCTION FOR REDUCE PRODUCT FROM LIST WHEN CLICKED ON MINIMUS BUTTON
+  const handel_ReduceProductFromList = (id) => {};
+
+  // FUNCTION FOR DELETE PRODUCT FROM LIST WHEN CLICKED ON DELETE BUTTON
+
+  const handel_DeleteProductFromList = (id) => {
+    const _handleRequestApi = async () => {
+      let params = {
+        product: id,
+        count: 1,
+      };
+      let loadData = null;
+      let dataUrl = `/cart2/api/cart_items/2/`;
+      let response = await ApiRegister().apiRequest(
+        loadData,
+        "DELETE",
+        dataUrl,
+        true,
+        params
+      );
+
+      if (response.status === 200) {
+      }
+    };
+
+    _handleRequestApi();
+  };
+
+  return (
+    <ContextProduct.Provider
+      value={{
+        All_product_list_buy: All_product_list_buy,
+        handel_AddProductTOList: handel_AddProductTOList,
+        handel_ReduceProductFromList: handel_ReduceProductFromList,
+        handel_DeleteProductFromList: handel_DeleteProductFromList,
+      }}
+    >
+      <div className="all">
+        <Head>
+          <link
+            rel="stylesheet"
+            href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+            integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+            crossorigin="anonymous"
+          />
+          <link
+            rel="stylesheet"
+            href="https://use.fontawesome.com/releases/v5.15.3/css/all.css"
+            integrity="sha384-SZXxX4whJ79/gErwcOYf+zWLeJdY/qpuqC4cAa9rOGUstPomtqpuNWT9wdPEn2fk"
+            crossorigin="anonymous"
+          ></link>
+        </Head>
+        <section className="container container--mob pb-5 ">
+          <CheckOutSteps step="1" />
+          <MiniCardBuy />
+
+          <div className="row mx-auto mt-4" style={{ maxWidth: "72rem" }}>
+            <ListCardBuy />
+            <SumBuy />
+          </div>
+        </section>
+      </div>
+    </ContextProduct.Provider>
   );
 }
 
