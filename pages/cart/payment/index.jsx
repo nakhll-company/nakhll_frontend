7be55694467913356c7fb9795c35dedc633f7a6b
SeyedHassan-Mahdavi <mahdaviali19@yahoo.com>
2021-08-22 14:36:21 +0430

    update css in page payment

--- pages/cart/payment/index.jsx
+++ pages/cart/payment/index.jsx
@@ -113,9 +113,8 @@ export default function Cart() {
       );
       setMsgCoupon(
         data.coupon &&
-          `کوپن شما با مبلغ ${
-            data.coupon_details.result / 10
-          } تومان برای شما فعال گردید.`
+        `کوپن شما با مبلغ ${data.coupon_details.result / 10
+        } تومان برای شما فعال گردید.`
       );
 
       setIsLoadInvoice(false);
@@ -218,6 +217,8 @@ export default function Cart() {
             </h2>
           </div>
           <div className={styles.steps_body}>
+
+
             {/* کد تخفیف */}
             <div>
               <h3
@@ -232,40 +233,29 @@ export default function Cart() {
               </h3>
               <form
                 style={{ borderRadius: "5px" }}
-                // className="input-group "
+                className={`input-group ${styles.input_group__box}`}
                 onSubmit={(e) => _addCoupon(e)}
               >
-                <div className="input-group ">
-                  <input
-                    type="text"
-                    name="coupon-code"
-                    placeholder="اگر کد تخفیف دارید، وارد کنید"
-                    className="form-control"
-                  />
+                <input
+                  type="text"
+                  name="coupon-code"
+                  placeholder="اگر کد تخفیف دارید، وارد کنید"
+                  className={`form-control ${styles.boxShadowNot}`}
+                />
+
+                <div className="input-group-append">
                   <button
                     type="submit"
-                    className="btn  rounded-pill px-3"
+                    className={`btn rounded-pill px-3 ${styles.btnCheckCoupon}`}
                     style={{
                       backgroundColor: "rgb(27,62,104)",
                       color: "#fff",
                     }}
                   >
                     بررسی
-                  </button>
-                  {/* <div className="input-group-append">
-                    <button
-                      type="submit"
-                      className="btn  rounded-pill px-3"
-                      style={{
-                        backgroundColor: "rgb(27,62,104)",
-                        color: "#fff",
-                      }}
-                    >
-                      بررسی
                     </button>
-                  </div> */}
                 </div>
-                <div className="input-group-bg rounded-pill"></div>
+                <div className={`${styles.input_group_bg} rounded-pill`}></div>
               </form>
               {msgCoupon !== "" && (
                 <div className="border border-success bg-white font-size-9 py-2 pr-3 mt-3 rounded d-flex flex-wrap justify-content-between align-items-center">
@@ -327,8 +317,8 @@ export default function Cart() {
             <div className="mt-3 position-relative">
               {/**/}
               {isLoadInvoice && (
-                <div className="loading-box">
-                  <div className="spinner spinner--tiny"></div>
+                <div className={styles.loading_box}>
+                  <div className={`${styles.spinner} ${styles.spinner__tiny}`}></div>
                 </div>
               )}
               {item &&
@@ -339,7 +329,7 @@ export default function Cart() {
                         از غرفه {itemProduct.product.shop.title}
                       </div>
                       <div className="d-flex align-items-center mt-3">
-                        <div className="avatar avatar-square">
+                        <div className={`${styles.picItemInvoice}`}>
                           <a href="/pestehkerman/product/175903" className>
                             <img
                               src="https://statics.basalam.com/public/users/j2ggy/1911/n41s9awDlcLoiKAF6oWQwo3MopX4cnDJzZHgE3Do.jpeg_256X256X70.jpeg"
@@ -347,7 +337,7 @@ export default function Cart() {
                             />
                           </a>
                         </div>
-                        <div className="mr-3" style={{ minWidth: "1%" }}>
+                        <div className="mr-3" style={{ minWidth: "1%" , marginRight:"1rem" }}>
                           <a
                             href={itemProduct.product.url}
                             className="link-body"
@@ -356,7 +346,7 @@ export default function Cart() {
                           </a>
                           <div className="mt-2">{itemProduct.count}عدد</div>
                         </div>
-                        <div className="mr-auto">
+                        <div className="mr-auto" style={{ marginRight:"auto" }}>
                           {_asist.PSeparator(itemProduct.total_price / 10)}{" "}
                           تومان
                         </div>
@@ -410,20 +400,20 @@ export default function Cart() {
               <div className="font-size-sm border-bottom py-3">
                 <div className="d-flex py-1">
                   <span>جمع بهای سفارش:</span>
-                  <span className="mr-auto">
+                  <span className="mr-auto" style={{marginRight:"auto"}}>
                     {_asist.PSeparator(totalPrice / 10)} تومان
                   </span>
                 </div>
                 <div className="d-flex py-1">
                   <span>هزینه ارسال:</span>{" "}
-                  <span className="mr-auto">
+                  <span className="mr-auto" style={{marginRight:"auto"}}>
                     {_asist.PSeparator(logisticPrice / 10)} تومان
                   </span>
                 </div>
                 {msgCoupon !== "" && (
                   <div className="d-flex py-1 text-danger">
                     <span>تخفیف قیمت محصولات (-) :</span>
-                    <span className="mr-auto">{resultCoupon} تومان</span>
+                    <span className="mr-auto" style={{marginRight:"auto"}}>{resultCoupon} تومان</span>
                   </div>
                 )}
                 {/**/} {/**/}
@@ -431,7 +421,7 @@ export default function Cart() {
                   <span style={{ color: "rgb(27,62,104)" }}>
                     مبلغ قابل پرداخت:
                   </span>
-                  <span className="mr-auto" style={{ color: "rgb(27,62,104)" }}>
+                  <span className="mr-auto" style={{ color: "rgb(27,62,104)" , marginRight:"auto" }}>
                     {_asist.PSeparator(finalPrice / 10)} تومان
                   </span>
                 </div>
@@ -439,13 +429,12 @@ export default function Cart() {
             </div>
 
             <div className="mt-3 p-3 border rounded font-size-sm my-3 line-height-normal">
-              <div className="mx-auto text-justify">
+              <div className="mx-auto" style={{textAlign:"center"}}>
                 تمامی بسته‌های پستی به آقا/خانم
                 <strong> {addressReceiver.receiver_full_name} </strong>
                 به آدرس
                 <strong className="font-size-8">
-                  {" "}
-                  {addressReceiver.address}{" "}
+                  {addressReceiver.address}
                 </strong>
                 تحویل داده می‌شوند.
               </div>
@@ -507,7 +496,7 @@ export default function Cart() {
               </span>
             </div>
 
-            <div className="buttonPayment mt-3 ">
+            <div className={`${styles.buttonPayment} mt-3`} >
               <div className="d-md-none mb-2 font-size-sm text-success d-flex">
                 <span
                   className="font-weight-bold"
@@ -525,7 +514,7 @@ export default function Cart() {
               </div>
               <button
                 style={{ backgroundColor: "rgb(27,62,104)", color: "#fff" }}
-                className="btn  cart-button w-100 d-flex justify-content-between align-items-center px-4"
+                className={`${styles.cart_button} btn w-100 d-flex justify-content-between align-items-center px-4`}
               >
                 <span className="d-inline-block w-100 text-center font-size1">
                   پرداخت آنلاین
