b725d8f168fb692bd46e53af55983010b2695161
miillaadddddd <milad7212@gmail.com>
2021-10-05 18:39:05 +0330

    feat (CardProduct): Change Place bookmark

--- components/ProductCart/ProductCard.jsx
+++ components/ProductCart/ProductCard.jsx
@@ -2,7 +2,7 @@
 import Assistent from "zaravand-assistent-number";
 // methods
 import { addToFavoritesList } from "./methods/addToFavotitesList";
-import { deleteFromFavoritesList } from './methods/deleteFromFavoritesList';
+import { deleteFromFavoritesList } from "./methods/deleteFromFavoritesList";
 // scss
 import styles from "./ProductCard.module.scss";
 
@@ -23,18 +23,17 @@ const ProductCard = ({
       <div
         className="_product_card_badge"
         type="button"
-        style={{ top: ".75rem" }}
+        style={{ bottom: ".75rem" }}
         onClick={() => {
           addToFavoritesList(product.id);
         }}
       >
         <i className="far fa-bookmark" />
-
       </div>
       <div
         className="_product_card_badge"
         type="button"
-        style={{ top: "3.3rem" }}
+        style={{ bottom: "3.3rem" }}
       >
         <i className="fas fa-share-alt"></i>
       </div>
@@ -44,31 +43,50 @@ const ProductCard = ({
   let cardImg = (
     <img
       src={product.imageUrl}
-      className={`card-img-top _product_card_rounded animationCart ${product.unavailable && "_unavailable_product"
-        }`}
+      className={`card-img-top _product_card_rounded animationCart ${
+        product.unavailable && "_unavailable_product"
+      }`}
       alt={product.title}
     />
   );
 
   return (
     <div
-      className={`animationCartParent ${col
-        ? `col-${col}`
-        : `col-6 col-sm-${sm} col-md-${md} col-lg-${lg} col-xl-${xl}`
-        } ${padding ? `px-${padding}` : ""} mb-3`}
+      className={`animationCartParent ${
+        col
+          ? `col-${col}`
+          : `col-6 col-sm-${sm} col-md-${md} col-lg-${lg} col-xl-${xl}`
+      } ${padding ? `px-${padding}` : ""} mb-3`}
     >
-      {product.iconClose && <span style={{ position: "relative", width: "0px", height: "0px", cursor: "pointer" }} onClick={() => {
-        deleteFromFavoritesList(product.id);
-      }}>
-        <i class="fa fa-times-circle" style={{ position: "absolute", fontSize: "28px", color: "#4f4f4f", zIndex: "100" }}></i>
-      </span>}
+      {product.iconClose && (
+        <span
+          style={{
+            position: "relative",
+            width: "0px",
+            height: "0px",
+            cursor: "pointer",
+          }}
+          onClick={() => {
+            deleteFromFavoritesList(product.id);
+          }}
+        >
+          <i
+            class="fa fa-times-circle"
+            style={{
+              position: "absolute",
+              fontSize: "28px",
+              color: "#4f4f4f",
+              zIndex: "100",
+            }}
+          ></i>
+        </span>
+      )}
       <div
         // style={{ minHeight: "170px" }}
         className="card _product_card _product_card_rounded p-2"
       >
-        {cardBadge}
-
         <div className={styles.paterImage}>
+          {cardBadge}
           <a href={product.url}>{cardImg}</a>
         </div>
         {/* {linkType === "anchor" ? (
@@ -82,8 +100,9 @@ const ProductCard = ({
         )} */}
 
         <div
-          className={`card-body mt-2 p-1 ${product.unavailable && "_unavailable_product"
-            }`}
+          className={`card-body mt-2 p-1 ${
+            product.unavailable && "_unavailable_product"
+          }`}
         >
           <div className=" mb-3">
             <a
@@ -191,7 +210,12 @@ const ProductCard = ({
                       {_asist.PSeparator(product.discountNumber)}
                     </span>
                   )}
-                  <span className="_product_card_toman" style={{ fontSize: ".75rem" }}>تومان</span>
+                  <span
+                    className="_product_card_toman"
+                    style={{ fontSize: ".75rem" }}
+                  >
+                    تومان
+                  </span>
                 </>
               )}
             </div>
