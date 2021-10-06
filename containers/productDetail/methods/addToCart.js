1d06b3fa9180e61589ea4452fa676c5805ddcb80
sahar <s.shafiee_73@yahoo.com>
2021-10-06 12:50:40 +0330

    Fix (productDetail) : add to cart

--- containers/productDetail/methods/addToCart.js
+++ containers/productDetail/methods/addToCart.js
@@ -2,16 +2,20 @@ import { ApiRegister } from "../../../services/apiRegister/ApiRegister";
 import { errorMessage, successMessage } from "../../utils/message";
 // big city
 export const addToCart = async (productId) => {
-    let response = await ApiRegister().apiRequest(
-        null,
-        "get",
-        `/cart2/api/cart_items/${productId}/add/`,
-        true,
-        {}
-    );
-    if (response.status === 200) {
-        successMessage("با موفقیت به سبد خرید شما اضافه شد");
-    } else {
-        errorMessage("خطایی پیش آماده");
+    try {
+        let response = await ApiRegister().apiRequest(
+            null,
+            "get",
+            `/cart2/api/cart_items/${productId}/add/`,
+            true,
+            {}
+        );
+        if (response.status === 201) {
+            successMessage("با موفقیت به سبد خرید شما اضافه شد");
+        } else {
+            errorMessage(response.response.data[0]);
+        }
+    } catch (error) {
+        errorMessage("خطایی در افزودن به سبد خرید پیش آمده");
     }
-};
+}
