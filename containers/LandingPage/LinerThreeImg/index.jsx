8fd1af62d4124f40f070354be9cc9df4dcf675da
miillaadddddd <milad7212@gmail.com>
2021-10-10 13:25:28 +0330

    fix (component_4) : in desktop

--- containers/LandingPage/LinerThreeImg/index.jsx
+++ containers/LandingPage/LinerThreeImg/index.jsx
@@ -27,45 +27,47 @@ function LinerThreeImg({ nextApi_LinerThreeImg }) {
     <>
       {dataLinerThreeImg.length >= 3 && (
         <>
-          <div className={styles.topImage}>
-            <a
-              href={dataLinerThreeImg[0].url}
-              title={dataLinerThreeImg[0].description}
-            >
-              <img
-                src={dataLinerThreeImg[0].image}
-                alt={dataLinerThreeImg[0].title}
-              />
-            </a>
+          <div className="container">
+            <div className={styles.topImage}>
+              <a
+                href={dataLinerThreeImg[0].url}
+                title={dataLinerThreeImg[0].description}
+              >
+                <img
+                  src={dataLinerThreeImg[0].image}
+                  alt={dataLinerThreeImg[0].title}
+                />
+              </a>
+            </div>
+            <aside className={styles.section}>
+              <a
+                href={dataLinerThreeImg[1].url}
+                data-observed="0"
+                target="_blank"
+                title={dataLinerThreeImg[1].description}
+                className={styles.one}
+              >
+                <img
+                  src={dataLinerThreeImg[1].image}
+                  loading="lazy"
+                  alt={dataLinerThreeImg[1].title}
+                />
+              </a>
+              <a
+                href={dataLinerThreeImg[2].url}
+                data-observed="0"
+                target="_blank"
+                title={dataLinerThreeImg[2].description}
+                className={styles.two}
+              >
+                <img
+                  src={dataLinerThreeImg[2].image}
+                  loading="lazy"
+                  alt={dataLinerThreeImg[2].title}
+                />
+              </a>
+            </aside>
           </div>
-          <aside className={styles.section}>
-            <a
-              href={dataLinerThreeImg[1].url}
-              data-observed="0"
-              target="_blank"
-              title={dataLinerThreeImg[1].description}
-              className={styles.one}
-            >
-              <img
-                src={dataLinerThreeImg[1].image}
-                loading="lazy"
-                alt={dataLinerThreeImg[1].title}
-              />
-            </a>
-            <a
-              href={dataLinerThreeImg[2].url}
-              data-observed="0"
-              target="_blank"
-              title={dataLinerThreeImg[2].description}
-              className={styles.two}
-            >
-              <img
-                src={dataLinerThreeImg[2].image}
-                loading="lazy"
-                alt={dataLinerThreeImg[2].title}
-              />
-            </a>
-          </aside>
         </>
       )}
     </>
