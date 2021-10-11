3d73c6fdda06a3d066cad60c2fb8e721bdd867c0
miillaadddddd <milad7212@gmail.com>
2021-10-11 17:46:46 +0330

    fix (component 3): fix in desktop and mobile

--- containers/LandingPage/LinerThreeImg/index.jsx
+++ containers/LandingPage/LinerThreeImg/index.jsx
@@ -7,7 +7,7 @@ function LinerThreeImg({ dataLinerThreeImg }) {
     <>
       {dataLinerThreeImg.length >= 3 && (
         <>
-          <div className="container">
+          <div className="container d-lg-none">
             <div className={styles.topImage}>
               <a
                 href={dataLinerThreeImg[0].url}
@@ -47,6 +47,52 @@ function LinerThreeImg({ dataLinerThreeImg }) {
                 />
               </a>
             </aside>
+          </div>
+
+          {/* for Desktop */}
+          <div className="container d-none d-lg-block">
+            <div className={`${styles.parent}  row`}>
+              <div className="col-9">
+                <a
+                  href={dataLinerThreeImg[0].url}
+                  title={dataLinerThreeImg[0].description}
+                  className={styles.right_img}
+                >
+                  <img
+                    src={dataLinerThreeImg[0].image}
+                    alt={dataLinerThreeImg[0].title}
+                  />
+                </a>
+              </div>
+              <div className={`${styles.left_side}   col-3`}>
+                <a
+                  href={dataLinerThreeImg[1].url}
+                  data-observed="0"
+                  target="_blank"
+                  title={dataLinerThreeImg[1].description}
+                  className={styles.left_image}
+                >
+                  <img
+                    src={dataLinerThreeImg[1].image}
+                    loading="lazy"
+                    alt={dataLinerThreeImg[1].title}
+                  />
+                </a>
+                <a
+                  href={dataLinerThreeImg[2].url}
+                  data-observed="0"
+                  target="_blank"
+                  title={dataLinerThreeImg[2].description}
+                  className={styles.left_image}
+                >
+                  <img
+                    src={dataLinerThreeImg[2].image}
+                    loading="lazy"
+                    alt={dataLinerThreeImg[2].title}
+                  />
+                </a>
+              </div>
+            </div>
           </div>
         </>
       )}
