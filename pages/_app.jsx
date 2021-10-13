97ad6edfc9986d1adc91fde73b2326e6d473e933
miillaadddddd <milad7212@gmail.com>
2021-10-13 15:25:58 +0330

    fix (general):fix code GoogleAnalytics

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -28,14 +28,14 @@ function MyApp({ Component, pageProps }) {
       <>
         <Script
           strategy="lazyOnload"
-          src={"https://www.googletagmanager.com/gtag/js?id=G-Z8E2Z09JDT"}
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
         />
         <Script strategy="lazyOnload">
           {`window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', 'G-Z8E2Z09JDT');`}
+  gtag('config', ${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS});`}
         </Script>
         <Provider store={Store}>
           <MyLayout>
