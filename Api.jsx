9e4fa31408f0f528a057f02fbc3920b4a23382a0
sahar <s.shafiee_73@yahoo.com>
2021-10-14 19:58:14 +0330

    Fix(landing): change api schema url

--- Api.jsx
+++ Api.jsx
@@ -1,5 +1,5 @@
 export const ApiReference = {
-  Landing_Page: "http://51.89.107.174:8000/api/v1/landing/schema/",
+  Landing_Page: "/api/v1/landing/schema/",
   menu: "/api/v1/markets/",
   shop: "/api/v1/shop/",
   schemaShop: "/api/v1/shop/schema/",
