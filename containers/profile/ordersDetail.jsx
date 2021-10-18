39ec979a7199b7a91d13c4285f1f384ccf6c57e1
sahar <s.shafiee_73@yahoo.com>
2021-10-18 13:11:53 +0330

    Fix(orderDetail): makes image factor status dynamic

--- containers/profile/ordersDetail.jsx
+++ containers/profile/ordersDetail.jsx
@@ -19,6 +19,9 @@ const OrdersDetail = ({ invoiceId }) => {
         await getOrderDetail(invoiceId, setDetailData, setLoading);
     }, []);
 
+    let jsonAddress = detailData.address_json || "{}";
+    jsonAddress = JSON.parse(jsonAddress);
+
     return (
         <div className={styles.main_wrapper}>
             <h6>جزئیات سفارش</h6>
@@ -42,41 +45,49 @@ const OrdersDetail = ({ invoiceId }) => {
                             <span>{_asist.PSeparator(`${detailData.final_price / 10} تومان`)}</span>
                         </div>
                         <div className={`${styles.price} p-3`}>
+                            <span className="text-secondary d-block mb-2">کوپن اعمال شده</span>
+                            <span>{_asist.PSeparator(`${detailData.coupons_total_price} تومان`)}</span>
+                        </div>
+                        <div className={`${styles.right} p-3`}>
+                            <span className="text-secondary d-block mb-2">هزینه ارسال</span>
+                            <span>{_asist.PSeparator(`${detailData.logistic_price / 10} تومان`)}</span>
+                        </div>
+                        <div className={`${styles.price} p-3`}>
                             <span className="text-secondary d-block mb-2">گیرنده</span>
-                            <span>{`${detailData.user.first_name} ${detailData.user.last_name}`}</span>
+                            <span>{`${detailData.items[0].buyer}`}</span>
                         </div>
                         <div className={`${styles.images} p-3`}>
-                            <span className="text-secondary mb-2"> آدرس :</span>
-                            <span>{detailData.address && detailData.address.address}</span>
+                            <span className="text-secondary mb-2"> آدرس گیرنده :</span>
+                            <span>{jsonAddress.state && `${jsonAddress.state} | ${jsonAddress.big_city} | ${jsonAddress.city} | ${jsonAddress.address}`}</span>
                         </div>
                         <div className={`${styles.detail} p-3`}>
                             <span className="text-secondary mb-2"> شماره موبایل :</span>
-                            <span>{detailData.address && _asist.number(detailData.address.receiver_mobile_number)}</span>
+                            <span>{jsonAddress && _asist.number(jsonAddress.receiver_mobile_number)}</span>
                         </div>
                     </div>
                     {detailData.items.length > 0 && detailData.items.map((value, index) => (
                         <div className={styles.list_items} key={index}>
                             <div style={{ flexBasis: "100%", padding: "10px", borderBottom: "1px solid #dfdfdf" }}>
-                                <h6>سفارش شماره {_asist.number(index + 1)}</h6>
+                                <h6>محصول شماره {_asist.number(index + 1)}</h6>
                             </div>
                             <div className={`${styles.right} p-3`}>
                                 <span className="d-block text-secondary mb-2">نام حجره</span>
                                 <span className="text-dark font-weight-bold">{value.shop_name}</span>
                             </div>
                             <div className={`${styles.price} p-3`}>
-                                <span className="text-secondary d-block mb-2">استان</span>
-                                <span>{detailData.address && `${detailData.address.big_city} / ${detailData.address.city}`}</span>
+                                <span className="text-secondary d-block mb-2">وزن محصول</span>
+                                <span>{_asist.number(value.weight)}</span>
                             </div>
                             <div className={`${styles.right} p-3`}>
-                                <span className="text-secondary d-block mb-2">مبلغ کل سفارش</span>
-                                <span>{_asist.PSeparator(`${detailData.final_price / 10} تومان`)}</span>
+                                <span className="text-secondary d-block mb-2">مبلغ</span>
+                                <span>{_asist.PSeparator(`${value.price_without_discount / 10} تومان`)}</span>
                             </div>
                             <div className={`${styles.price} p-3`}>
-                                <span className="text-secondary d-block mb-2">هزینه ارسال سفارش</span>
-                                <span>{_asist.PSeparator(`${detailData.logistic_price / 10} تومان`)}</span>
+                                <span className="text-secondary d-block mb-2">مبلغ با تخفیف</span>
+                                <span>{_asist.PSeparator(`${value.price_with_discount / 10} تومان`)}</span>
                             </div>
                             <div className={`${styles.images} p-3`}>
-                                <span className="text-secondary mb-2"> محصولات :</span>
+                                <span className="text-secondary mb-2"> محصول :</span>
                                 <div className="d-flex align-items-center">
                                     <Link href={`productDetail/${value.id}`}>
                                         <a>
@@ -90,7 +101,7 @@ const OrdersDetail = ({ invoiceId }) => {
                                             </a>
                                         </Link>
                                         <div className="">
-                                            <span className="mx-4">{_asist.PSeparator(`${value.price_with_discount / 10} تومان`)}</span>
+                                            {/* <span className="mx-4">{_asist.PSeparator(`${value.price_with_discount / 10} تومان`)}</span> */}
                                             <span>{_asist.number(value.count)} عدد</span>
                                         </div>
                                     </div>
@@ -100,31 +111,33 @@ const OrdersDetail = ({ invoiceId }) => {
                             <div className={`${styles.detail} p-3`}>
                                 <div className="d-flex justify-content-center">
                                     <div className="d-flex flex-column">
-                                        <Image src="/orderDetial/tik.svg" alt="" width="30" height="30" />
+                                        <Image src={detailData.status === "wait_store_approv" ? "/orderDetial/tik_black.svg" : "/orderDetial/tik.svg"} alt="" width="30" height="30" />
                                         <span className="mt-2">تایید حجره دار</span>
                                     </div>
                                     <div className="d-flex align-items-start">
                                         <hr />
-                                        <Image src="/orderDetial/box.svg" alt="" width="35" height="35" />
+                                        <Image src={detailData.status === "wait_store_approv" ? "/orderDetial/box_black.svg" : "/orderDetial/box.svg"} alt="" width="35" height="35" />
                                         <hr />
                                     </div>
                                     <div className="d-flex flex-column">
-                                        <Image src="/orderDetial/tik.svg" alt="" width="30" height="30" />
+                                        <Image src={(detailData.status === "wait_store_checkout" || detailData.status === "completed" || detailData.status === "wait_customer_approv") ? "/orderDetial/tik.svg" : "/orderDetial/tik_black.svg"} alt="" width="30" height="30" />
                                         <span className="mt-2">آماده سازی سفارش</span>
                                     </div>
                                     <div className="d-flex align-items-start">
                                         <hr />
-                                        <Image src="/orderDetial/car.svg" alt="" width="35" height="35" />
+                                        <Image src={(detailData.status === "wait_store_checkout" || detailData.status === "completed" || detailData.status === "wait_customer_approv") ? "/orderDetial/car.svg" : "/orderDetial/truke_black.svg"} alt="" width="35" height="35" />
                                         <hr />
                                     </div>
                                     <div className="d-flex flex-column">
-                                        <Image src="/orderDetial/tik.svg" alt="" width="30" height="30" />
+                                        <Image src={(detailData.status === "wait_store_checkout" || detailData.status === "completed") ? "/orderDetial/tik.svg" : "/orderDetial/tik_black.svg"} alt="" width="30" height="30" />
                                         <span className="mt-2">تایید مشتری</span>
                                     </div>
                                 </div>
                                 <div className="d-flex flex-column align-items-center mt-4">
-                                    کد رهگیری سفارش شما : {_asist.number(detailData.payment_request_datetime)}<br />
-                                    <Link href={`https://tracking.post.ir/?id=${detailData.payment_request_datetime}`}>
+                                    <span style={{ textAlign: "center" }}>
+                                        کد رهگیری سفارش شما : {_asist.number(value.barcode)}
+                                    </span><br />
+                                    <Link href={`https://tracking.post.ir/?id=${value.barcode}`}>
                                         <a target="_blank" style={{ border: "1px solid gray", borderRadius: "25px", padding: "5px 10px" }}>پیگیری سفارش در سامانه پست</a>
                                     </Link>
                                 </div>
