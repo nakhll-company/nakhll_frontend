f8ddfb2b1fbfc47fdc471d245378dda451d6cc3e
miillaadddddd <milad7212@gmail.com>
2021-10-23 16:11:53 +0330

    Feat(component):Add  Sm_LinerOneImg component

---
+++ components/SampelComponents/InputPlace/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./InputPlace.module.scss";
+
+function Sm_InputPlace(props) {
+  return <div>hi</div>;
+}
+
+export default Sm_InputPlace;
--- components/liveEdit/living/index.jsx
+++ components/liveEdit/living/index.jsx
@@ -6,45 +6,38 @@ import { ApiRegister } from "../../../services/apiRegister/ApiRegister";
 import styles from "./living.module.scss";
 import HeroSlides from "../../../containers/LandingPage/HeroSlides";
 import Sm_HeroSlides from "../../SampelComponents/HeroSlides";
+import Sm_InputPlace from "../../SampelComponents/InputPlace";
+import Sm_LinerOneImg from "../../SampelComponents/Sm_LinerOneImg";
 function Living(props) {
   const [characters, setCharacters] = useState([]);
   const [allshops, setAllshops] = useState([]);
-
+  const Sample = {
+    1: "اسلایدر تکی",
+    2: "بنر تک عکسی",
+    3: " بنر 2تایی در یک ردیف",
+    4: " (یکی بالا دوتا پایین)بنر ۳ تایی",
+    5: " بنر چهارتایی چهارتا کنار هم",
+    6: " ردیف محصولات",
+    7: " ردیف شگفت انگیزا",
+  };
   const list = [
     {
+      type: 0,
       ID: "46153726-3f09-4bb1-967c-ebd55c9751ba",
       slug: "mohammadi",
       title: "محمدی",
     },
     {
+      type: 1,
       ID: "f3501a78-2b0e-4302-9d1c-f282daa5592e",
       slug: "Roya",
       title: "رویا",
     },
     {
+      type: 2,
       ID: "c226da83-9526-465a-97d4-9f112a0dc636",
       slug: "Irana",
       title: "نقش و نگار",
-    },
-    {
-      ID: "abd61fbc-1d55-49a2-bd86-d25d42f706cb",
-      slug: "Royal",
-      title: "رویال",
-    },
-    {
-      ID: "82761df2-53a9-4512-aa51-e84e22bdcc3e",
-      slug: "Vahid-Trading",
-      title: "بازرگانی وحید",
-    },
-    {
-      ID: "95489838-27ed-48c1-abe7-c7cb145cb867",
-      slug: "mishka",
-      title: "میشکا",
-    },
-    {
-      ID: "e8fcadac-2a50-46b8-b2de-d3af69643a26",
-      slug: "Atelier-Anna",
-      title: "هنرکده آنا",
     },
   ];
   const dataSlider = [
@@ -92,6 +85,19 @@ function Living(props) {
     items.splice(result.destination.index, 0, reorderedItem);
     setCharacters(items);
   };
+  const _handel_select_component = (data, index) => {
+    switch (data.type) {
+      case 0:
+        return <Sm_InputPlace />;
+        break;
+      case 1:
+        return <Sm_HeroSlides />;
+        break;
+      case 2:
+        return <Sm_LinerOneImg />;
+        break;
+    }
+  };
 
   return (
     <DragDropContext onDragEnd={handleOnDragEnd}>
@@ -131,7 +137,7 @@ function Living(props) {
                       </button>
                     </div>
                     {/* fas fa-calendar-times" */}
-                    <Sm_HeroSlides />
+                    {_handel_select_component(e, index)}
 
                     {/* <HeroSlides dataHeroSlides={dataSlider}/> */}
                   </div>
