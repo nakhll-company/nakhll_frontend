36476f934f5d7a726bd97a6c832975bd58fd29f2
miillaadddddd <milad7212@gmail.com>
2021-10-23 12:41:46 +0330

    fix(url ):fix url in linear product

--- pages/hojreh/index.jsx
+++ pages/hojreh/index.jsx
@@ -74,7 +74,6 @@ const fetchData = async (id) => {
 };
 
 const Hojreh = ({ dataShop, data }) => {
- 
   const [informationShop, setInformationShop] = useState(dataShop.shop);
   const _handel_select_component = (type, index) => {
     switch (type.component_type) {
@@ -190,7 +189,6 @@ export default Hojreh;
 // function server side
 export async function getServerSideProps(context) {
   const dataShop = await fetchData(context.query.shop);
-  
 
   return {
     props: {
