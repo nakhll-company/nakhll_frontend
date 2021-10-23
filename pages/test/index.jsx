f06b630a2ee8358c194fdbd042894bd6f789c6f0
miillaadddddd <milad7212@gmail.com>
2021-10-23 11:53:55 +0330

    fix (liveEdit):

--- pages/test/index.jsx
+++ pages/test/index.jsx
@@ -1,28 +1,9 @@
 import React from "react";
+
 import styles from "./test.module.scss";
 
 function index(props) {
-  return (
-    <div className={styles.content}>
-      <div className={styles.slider}>
-        <div className={styles.right}>
-          <img src="/image/slide/slideLeft2.jpg" alt="" />
-        </div>
-        <div className={styles.left}>
-          <div className={styles.top}>
-              <a href="">
-                <img src="/image/slide/slidB.jpg" alt="" />
-              </a>
-          </div>
-          <div className={styles.bottom}>
-              <a href="">
-                <img src="/image/slide/slideLeft2.jpg" alt="" />
-              </a>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+  return <div className={styles.content}></div>;
 }
 
 export default index;
