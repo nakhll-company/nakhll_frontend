75b47eea4b1b9d252bf0a7d8353ed481bb5895ac
miillaadddddd <milad7212@gmail.com>
2021-10-24 14:00:24 +0330

    Feat(Component) : Add placeInput Component

--- components/SampelComponents/InputPlace/index.jsx
+++ components/SampelComponents/InputPlace/index.jsx
@@ -2,7 +2,22 @@ import React from "react";
 import styles from "./InputPlace.module.scss";
 
 function Sm_InputPlace(props) {
-  return <div>hi</div>;
+  return (
+    <>
+      <div className={styles.box_outer}>
+        <div className={styles.main_box}>
+          <div className={`${styles.bar} ${styles.top}`}></div>
+          <div
+            className={`${styles.bar} ${styles.right} ${styles.delay}`}
+          ></div>
+          <div
+            className={`${styles.bar} ${styles.bottom} ${styles.delay}`}
+          ></div>
+          <div className={`${styles.bar} ${styles.left}`}></div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Sm_InputPlace;
