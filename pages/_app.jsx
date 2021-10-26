766aadbdb1a982ed16e77db211cb7a3c02766982
sahar <s.shafiee_73@yahoo.com>
2021-10-26 16:07:04 +0330

    Feat(app): remove layout

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -56,7 +56,6 @@ function MyApp({ Component, pageProps }) {
           `,
           }}
         />
-
         <Script id="yektanet" strategy="lazyOnload">
           {`!function (t, e, n) {
         t.yektanetAnalyticsObject = n, t[n] = t[n] || function () {
@@ -77,6 +76,49 @@ function MyApp({ Component, pageProps }) {
           <MyLayout>
             <Component {...pageProps} />
           </MyLayout>
+        </Provider>
+      </>
+    );
+  } else if (router.pathname.startsWith("/liveEdit")) {
+    return (
+      <>
+        {/* Global Site Tag (gtag.js) - Google Analytics */}
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=UA-156540827-1`}
+        />
+        <Script
+          id="gtag-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'UA-156540827-1', {
+              page_path: window.location.pathname,
+            });
+          `,
+          }}
+        />
+        <Script
+          id="raychat"
+          strategy="lazyOnload"
+        >{`!function(){function t(){var t=document.createElement("script");t.type="text/javascript",t.async=!0,localStorage.getItem("rayToken")?t.src="https://app.raychat.io/scripts/js/"+o+"?rid="+localStorage.getItem("rayToken")+"&href="+window.location.href:t.src="https://app.raychat.io/scripts/js/"+o+"?href="+window.location.href;var e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(t,e)}var e=document,a=window,o="1b3710ed-495e-4794-bbc7-842af6728c48";"complete"==e.readyState?t():a.attachEvent?a.attachEvent("onload",t):a.addEventListener("load",t,!1)}();`}</Script>
+        <Script id="yektanet" strategy="lazyOnload">
+          {`!function (t, e, n) {
+        t.yektanetAnalyticsObject = n, t[n] = t[n] || function () {
+            t[n].q.push(arguments)
+        }, t[n].q = t[n].q || [];
+        var a = new Date, r = a.getFullYear().toString() + "0" + a.getMonth() + "0" + a.getDate() + "0" + a.getHours(),
+            c = e.getElementsByTagName("script")[0], s = e.createElement("script");
+        s.id = "ua-script-lPejcfYM"; s.dataset.analyticsobject = n;
+        s.async = 1; s.type = "text/javascript";
+        s.src = "https://cdn.yektanet.com/rg_woebegone/scripts_v3/lPejcfYM/rg.complete.js?v=" + r, c.parentNode.insertBefore(s, c)
+    }(window, document, "yektanet");`}
+        </Script>
+        <Provider store={Store}>
+          <Component {...pageProps} />
         </Provider>
       </>
     );
