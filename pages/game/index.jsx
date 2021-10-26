34877a2f65af125129d1edcd757522102ccd7385
miillaadddddd <milad7212@gmail.com>
2021-10-26 18:51:17 +0330

    Feat(Game)

--- pages/game/index.jsx
+++ pages/game/index.jsx
@@ -1,7 +1,40 @@
 import React from "react";
-
+import styles from "./game.module.scss";
 function index(props) {
-  return <div></div>;
+  return (
+    <>
+      <>
+        <div className={styles.body}>
+          <div
+            id="card"
+            className={styles.card}
+            onClick={() => {
+              const card = document.querySelector("#card");
+
+              card.addEventListener("click", () => {
+                card.classList.toggle("flip-card");
+              });
+            }}
+          >
+            <div className={styles.front}>
+              <div className={styles.circle}></div>
+              <div className={styles.triangle}></div>
+              <div className={styles.square}></div>
+            </div>
+            <div className={styles }>
+              <div className={styles.icon}></div>
+              <div className={styles.phone_number}>8650 4006</div>
+            </div>
+          </div>
+        </div> 
+        <style jsx>{`
+          .flip-card {
+            transform: rotateY(180deg);
+          }
+        `}</style>
+      </>
+    </>
+  );
 }
 
 export default index;
