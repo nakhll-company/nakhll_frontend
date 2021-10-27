8ec0c415a263a43bce9bb8bae0eae8ca9c4e276a
miillaadddddd <milad7212@gmail.com>
2021-10-27 12:03:23 +0330

    Feat(EditPage):Add component in empty place

--- containers/liveEdit/ListComponent/index.jsx
+++ containers/liveEdit/ListComponent/index.jsx
@@ -7,39 +7,58 @@ import Sm_LinerThreeImg from "../../../components/SampelComponents/Sm_LinerThree
 import Sm_LinerTwoImg from "../../../components/SampelComponents/Sm_LinerTwoImg";
 import styles from "./ListComponent.module.scss";
 
-
-function ListComponent(props) {
+function ListComponent({ _handel_add_component }) {
   return (
     <>
       <div className={styles.parent}>
-        <div className={styles.holderItems}>
-          <Sm_LinerThreeImg />
-          <div className={styles.cover}></div>
-        </div>
-        <div className={styles.holderItems}>
+        <div
+          className={styles.holderItems}
+          onClick={() => _handel_add_component(<Sm_HeroSlides />,  1)}
+        >
           <Sm_HeroSlides />
           <div className={styles.cover}></div>
         </div>
-        <div className={styles.holderItems}>
-          <Sm_LinerFourImg />
+        <div
+          className={styles.holderItems}
+          onClick={() => _handel_add_component(<Sm_LinerOneImg />,  2)}
+        >
+          <Sm_LinerOneImg />
           <div className={styles.cover}></div>
         </div>
-        <div className={styles.holderItems}>
-          <Sm_LinerProducts />
-          <div className={styles.cover}></div>
-        </div>
-        <div className={styles.holderItems}>
+        <div
+          className={styles.holderItems}
+          onClick={() => _handel_add_component(<Sm_LinerTwoImg />,  3)}
+        >
           <Sm_LinerTwoImg />
           <div className={styles.cover}></div>
         </div>
-        <div className={styles.holderItems}>
-          <Sm_LinerOneImg />
+        <div
+          className={styles.holderItems}
+          onClick={() =>
+            _handel_add_component(<Sm_LinerThreeImg />, 4)
+          }
+        >
+          <Sm_LinerThreeImg />
           <div className={styles.cover}></div>
         </div>
-        <div className={styles.holderItems}>
-          <Sm_LinerOneImg />
+
+        <div
+          className={styles.holderItems}
+          onClick={() => _handel_add_component(<Sm_LinerFourImg />,  5)}
+        >
+          <Sm_LinerFourImg />
           <div className={styles.cover}></div>
         </div>
+        <div
+          className={styles.holderItems}
+          onClick={() =>
+            _handel_add_component(<Sm_LinerProducts />,  6)
+          }
+        >
+          <Sm_LinerProducts />
+          <div className={styles.cover}></div>
+        </div>
+
         <div style={{ marginTop: "30px" }}></div>
       </div>
     </>
