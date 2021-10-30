03eeba15408ab11f2a92fc8a91620f5412a7695b
miillaadddddd <milad7212@gmail.com>
2021-10-31 00:08:45 +0330

    Feat(Sgame):three part

--- pages/game/three/index.jsx
+++ pages/game/three/index.jsx
@@ -1,38 +1,44 @@
 import React, { useEffect, useRef } from "react";
 import styles from "./game.module.scss";
 import Link from "next/link";
+import { gsap, Power3 } from "gsap";
 function index(props) {
-  let btn = useRef(null);
-  let btn2 = useRef(null);
+  let onePart = useRef(null);
   return (
     <>
-      <div className={styles.onePage}>
-        دنیای کسب و کار مثل یه بازی می‌مونه، باید اهل رقابت و رفاقت باشی و
-        بپذیری که این بازی، باخت هم داره! حالا در کنار رقابت سالمی که صاحبان کسب
-        و کار با همدیگه دارن، دلال‌هایی هم هستن که به دور از اخلاق و انسانیت،
-        فقط به فکر سود خودشونن و براشون هیچ مهم نیست که با کارشون چه بلایی سر
-        بازار و کسب و کارها میارن. 
+      <div ref={(el) => (onePart = el)} className={styles.onePage}>
+        <span>دنیای کسب و کار مثل یه بازی می‌مونه،</span>
+        <br/>
+        <span>باید اهل رقابت و رفاقت باشی</span> <br/>
+        <span>بپذیری که این بازی، باخت هم داره!</span> <br/>
+        <span>حالا در کنار رقابت
+        سالمی که صاحبان کسب و کار با همدیگه دارن</span> <br/>
+        <span>دلال‌هایی هم هستن که به دور از
+        اخلاق و انسانیت، فقط به فکر سود خودشونن و براشون هیچ مهم نیست که با
+        کارشون چه بلایی سر بازار و کسب و کارها میار</span> <br/>
+        
+      .
       </div>
       <div className={styles.twoPage}>
-      بازی توی بازار آنلاین نخل اما دست دلال‌ها رو
-        کوتاه می‌کنه و باخت نداره. بازی‌ای که نه طاقت‌فرساس، نه هزینه‌ای داره و
-        نه کسی متضرر میشه. اینجا تو وارد رفاقت میشی که با دوستای جدیدت بتونی
-        شبکه‌ی بزرگتری از جامعه در سراسر کشور برای خودت بسازی و کسب و کارت رو با
-        این جامعه رشد بدی. «اینجوری همه‌جای ایران، سرای تو میشه...»
-
+        <span></span>
+        بازی توی بازار آنلاین نخل اما دست دلال‌ها رو کوتاه می‌کنه و باخت نداره.
+        بازی‌ای که نه طاقت‌فرساس، نه هزینه‌ای داره و نه کسی متضرر میشه. اینجا تو
+        وارد رفاقت میشی که با دوستای جدیدت بتونی شبکه‌ی بزرگتری از جامعه در
+        سراسر کشور برای خودت بسازی و کسب و کارت رو با این جامعه رشد بدی.
+        «اینجوری همه‌جای ایران، سرای تو میشه...»
       </div>
       <div className={styles.threePage}>
         <div className={styles.wrap}>
           <Link href="/game/four">
             <a>
-              <div ref={(el) => (btn = el)} className={styles.btnYes}>
+              <div className={styles.btnYes}>
                 <button>بله</button>
               </div>
             </a>
           </Link>
           <Link href="/game/No">
             <a>
-              <div ref={(el) => (btn2 = el)} className={styles.btnNo}>
+              <div className={styles.btnNo}>
                 <button>خیر</button>
               </div>
             </a>
--- pages/game/two/index.jsx
+++ pages/game/two/index.jsx
@@ -1,12 +1,23 @@
 import React, { useEffect, useRef } from "react";
 import styles from "./game.module.scss";
+import { gsap, Power3 } from "gsap";
 import Link from "next/link";
 function index(props) {
+  let tl = new gsap.timeline();
+  let all =useRef(null)
   let btn =useRef(null)
   let btn2 =useRef(null)
+  useEffect(() => {
+    // for Animation
+    tl.from(all,{
+      opacity:0,
+      duration:2,
+    }).from(btn, { x:200
+       }).from(btn2,{x:-200})
+  }, []);
   return (
     <>
-      <div className={styles.onePage}>
+      <div ref={(el)=>all=el} className={styles.onePage}>
       <div className={styles.wrap}>
         <Link href="/game/three">
             <a>
