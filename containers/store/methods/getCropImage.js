38c1e9f3450e7d4d32d7db798356b51427f994f8
sahar <s.shafiee_73@yahoo.com>
2021-10-30 15:58:22 +0330

    Feat(cropImage): complete image cropper

--- containers/store/methods/getCropImage.js
+++ containers/store/methods/getCropImage.js
@@ -1,8 +1,8 @@
-const createImage = url =>
+const createImage = (url) =>
     new Promise((resolve, reject) => {
         const image = new Image()
         image.addEventListener('load', () => resolve(image))
-        image.addEventListener('error', error => reject(error))
+        image.addEventListener('error', (error) => reject(error))
         image.setAttribute('crossOrigin', 'anonymous') // needed to avoid cross-origin issues on CodeSandbox
         image.src = url
     })
@@ -58,9 +58,35 @@ export async function getCroppedImg(imageSrc, pixelCrop, rotation = 0) {
     // return canvas.toDataURL('image/jpeg');
 
     // As a blob
-    return new Promise(resolve => {
-        canvas.toBlob(file => {
+    return new Promise((resolve) => {
+        canvas.toBlob((file) => {
             resolve(URL.createObjectURL(file))
-        }, 'image/jpeg')
+        }, 'image/png')
+    })
+}
+
+export async function getRotatedImage(imageSrc, rotation = 0) {
+    const image = await createImage(imageSrc)
+    const canvas = document.createElement('canvas')
+    const ctx = canvas.getContext('2d')
+
+    const orientationChanged =
+        rotation === 90 || rotation === -90 || rotation === 270 || rotation === -270
+    if (orientationChanged) {
+        canvas.width = image.height
+        canvas.height = image.width
+    } else {
+        canvas.width = image.width
+        canvas.height = image.height
+    }
+
+    ctx.translate(canvas.width / 2, canvas.height / 2)
+    ctx.rotate((rotation * Math.PI) / 180)
+    ctx.drawImage(image, -image.width / 2, -image.height / 2)
+
+    return new Promise((resolve) => {
+        canvas.toBlob((file) => {
+            resolve(URL.createObjectURL(file))
+        }, 'image/png')
     })
 }
--- pages/fp/store/index.jsx
+++ pages/fp/store/index.jsx
@@ -1,53 +1,74 @@
 // node libraies
 import Image from 'next/image';
-import { useState, useCallback } from 'react';
 import Cropper from 'react-easy-crop';
+import { useState, useCallback } from 'react';
+// components
+import CustomModal from '../../../components/custom/customModal';
 // methods
 import { getCroppedImg } from '../../../containers/store/methods/getCropImage';
+import { selectImage } from '../../../containers/store/methods/selectImage';
+// scss
+import styles from './imageCrop.module.scss';
 
 const Demo = () => {
-    const [crop, setCrop] = useState({ x: 0, y: 0 })
-    const [rotation, setRotation] = useState(0)
-    const [zoom, setZoom] = useState(1)
-    const [croppedAreaPixels, setCroppedAreaPixels] = useState(null)
-    const [croppedImage, setCroppedImage] = useState(null)
+
+    const [showModal, setShowModal] = useState(false);
+    const [imageSrc, setImageSrc] = useState(null);
+    const [crop, setCrop] = useState({ x: 0, y: 0 });
+    const [rotation, setRotation] = useState(0);
+    const [zoom, setZoom] = useState(1);
+    const [croppedAreaPixels, setCroppedAreaPixels] = useState(null);
+    const [croppedImage, setCroppedImage] = useState(null);
 
     const onCropComplete = useCallback((croppedArea, croppedAreaPixels) => {
-        setCroppedAreaPixels(croppedAreaPixels)
+        setCroppedAreaPixels(croppedAreaPixels);
     }, []);
 
     const showCroppedImage = useCallback(async () => {
         try {
             const croppedImage = await getCroppedImg(
-                "https://img.huffingtonpost.com/asset/5ab4d4ac2000007d06eb2c56.jpeg?cache=sih0jwle4e&ops=1910_1000",
+                imageSrc,
                 croppedAreaPixels,
                 rotation
-            )
-            console.log('donee', { croppedImage })
-            setCroppedImage(croppedImage)
+            );
+            setCroppedImage(croppedImage);
         } catch (e) {
-            console.error(e)
+            console.error(e);
         }
-    }, [croppedAreaPixels, rotation])
+    }, [imageSrc, croppedAreaPixels, rotation]);
 
-    showCroppedImage();
     return (
-        <>
-            <div style={{ position: "relative" }}>
-                <Cropper
-                    image="https://img.huffingtonpost.com/asset/5ab4d4ac2000007d06eb2c56.jpeg?cache=sih0jwle4e&ops=1910_1000"
-                    crop={crop}
-                    rotation={rotation}
-                    zoom={zoom}
-                    aspect={4 / 3}
-                    onCropChange={setCrop}
-                    onRotationChange={setRotation}
-                    onCropComplete={onCropComplete}
-                    onZoomChange={setZoom}
-                />
-            </div>
-            {croppedImage && <Image src={croppedImage} alt="iamge crop" width="100" height="100" />}
-        </>
+        <div>
+            <label htmlFor="uploadIamge" className={styles.label_image} style={{ backgroundImage: `url(${croppedImage})` }}>
+                <i className="fas fa-plus"></i>
+                <span>انتخاب کنید</span>
+                <input type="file" id="uploadIamge" name="uploadIamge" onChange={(e) => { selectImage(e, setImageSrc, setShowModal) }} accept="image/*" style={{ visibility: "hidden" }} />
+            </label>
+            <CustomModal show={showModal} onClose={() => { setShowModal(false) }} content={
+                <div className={styles.modal_wrapper}>
+                    <div className={styles.cropper_wrapper}>
+                        <Cropper
+                            image={imageSrc}
+                            crop={crop}
+                            rotation={rotation}
+                            zoom={zoom}
+                            aspect={2 / 1}
+                            onCropChange={setCrop}
+                            onRotationChange={setRotation}
+                            onCropComplete={onCropComplete}
+                            onZoomChange={setZoom}
+                        />
+                    </div>
+                    <div className={styles.button_submit}>
+                        <button onClick={() => {
+                            showCroppedImage();
+                            setShowModal(false);
+                        }}>تایید</button>
+                    </div>
+                </div>
+            } />
+            {croppedImage && <Image src={croppedImage} alt="image crop" width="100" height="100" />}
+        </div>
     )
 }
 // export
