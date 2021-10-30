8177203c775af625f9bcf21ca3515ae669d47ccd
sahar <s.shafiee_73@yahoo.com>
2021-10-30 11:15:33 +0330

    Feat(store): add image cropper

--- pages/fp/store/index.jsx
+++ pages/fp/store/index.jsx
@@ -1,9 +1,54 @@
+// node libraies
+import Image from 'next/image';
+import { useState, useCallback } from 'react';
+import Cropper from 'react-easy-crop';
+// methods
+import { getCroppedImg } from '../../../containers/store/methods/getCropImage';
 
+const Demo = () => {
+    const [crop, setCrop] = useState({ x: 0, y: 0 })
+    const [rotation, setRotation] = useState(0)
+    const [zoom, setZoom] = useState(1)
+    const [croppedAreaPixels, setCroppedAreaPixels] = useState(null)
+    const [croppedImage, setCroppedImage] = useState(null)
 
-export default function Store() {
+    const onCropComplete = useCallback((croppedArea, croppedAreaPixels) => {
+        setCroppedAreaPixels(croppedAreaPixels)
+    }, []);
+
+    const showCroppedImage = useCallback(async () => {
+        try {
+            const croppedImage = await getCroppedImg(
+                "https://img.huffingtonpost.com/asset/5ab4d4ac2000007d06eb2c56.jpeg?cache=sih0jwle4e&ops=1910_1000",
+                croppedAreaPixels,
+                rotation
+            )
+            console.log('donee', { croppedImage })
+            setCroppedImage(croppedImage)
+        } catch (e) {
+            console.error(e)
+        }
+    }, [croppedAreaPixels, rotation])
+
+    showCroppedImage();
     return (
-        <div>
-            <h1>store</h1>
-        </div>
-    );
+        <>
+            <div style={{ position: "relative" }}>
+                <Cropper
+                    image="https://img.huffingtonpost.com/asset/5ab4d4ac2000007d06eb2c56.jpeg?cache=sih0jwle4e&ops=1910_1000"
+                    crop={crop}
+                    rotation={rotation}
+                    zoom={zoom}
+                    aspect={4 / 3}
+                    onCropChange={setCrop}
+                    onRotationChange={setRotation}
+                    onCropComplete={onCropComplete}
+                    onZoomChange={setZoom}
+                />
+            </div>
+            {croppedImage && <Image src={croppedImage} alt="iamge crop" width="100" height="100" />}
+        </>
+    )
 }
+// export
+export default Demo;
