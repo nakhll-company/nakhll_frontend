5591b14ffd2a75a749e977f60880ee4192d82072
miillaadddddd <milad7212@gmail.com>
2021-10-31 17:55:07 +0330

    Feat(sGame):Add last page

--- pages/game/finish/index.jsx
+++ pages/game/finish/index.jsx
@@ -1,19 +1,29 @@
 import React, { useEffect, useRef } from "react";
+import { gsap, Power3 } from "gsap";
 import styles from "./game.module.scss";
 import Link from "next/link";
 function index(props) {
-  let btn =useRef(null)
-  let btn2 =useRef(null)
+  let tl = new gsap.timeline();
+  let box1 = useRef(null);
+  let box2 = useRef(null);
+  useEffect(() => {
+    // for Animation
+    tl.from(box1, {
+      opacity: 0,
+      duration: 3,
+    }).from(box2, {
+      opacity: 0,
+      scale: 0,
+      ease: "back",
+      duration: 2,
+      delay: 1,
+    });
+  }, []);
   return (
     <>
-      <div className={styles.onePage}>
-      
-      </div>
-      <div className={styles.twoPage}>
-      
-      </div>
-      <div className={styles.threePage}>
-      
+      <div className={styles.wrap}>
+        <div ref={(el) => (box1 = el)} className={styles.onePage}></div>
+        <div ref={(el) => (box2 = el)} className={styles.twoPage}></div>
       </div>
     </>
   );
