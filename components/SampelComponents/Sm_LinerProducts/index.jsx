f4b9f4327af9fd10e3b1985912465513181cce32
miillaadddddd <milad7212@gmail.com>
2021-10-31 12:59:54 +0330

    Feat(EditPage):add change text in linear product component

--- components/SampelComponents/Sm_LinerProducts/index.jsx
+++ components/SampelComponents/Sm_LinerProducts/index.jsx
@@ -1,15 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Sm_LinerProducts.module.scss";
 
 import Sm_product from "../Sm_product";
 function Sm_LinerProducts(props) {
+  const [toggle, setToggle] = useState(true);
+  const [name, setName] = useState("پرفروش ترین");
+  const [toggleSubTitle, setToggleSubTitle] = useState(true);
+  const [subTitle, setSubTitle] = useState("زیرعنوان");
   return (
     <div className={styles.main}>
       <div className={styles.title}>
         <div className={styles.name}>
-          <span className={styles.mainTitle}>پرفروش ترین</span>
+          {toggle ? (
+            <span
+              className={styles.mainTitle}
+              onDoubleClick={() => {
+                setToggle(false);
+              }}
+            >
+              {name}
+            </span>
+          ) : (
+            <input
+              className={styles.input}
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              onKeyDown={(event) => {
+                if (event.key === "Enter" || event.key === "Escape") {
+                  setToggle(true);
+                  event.preventDefault();
+                  event.stopPropagation();
+                }
+              }}
+            />
+          )}
 
-          <span className={styles.subTitle}>منتخب</span>
+          {toggleSubTitle ? (
+            <span
+              className={styles.subTitle}
+              onDoubleClick={() => {
+                setToggleSubTitle(false);
+              }}
+            >
+              {subTitle}
+            </span>
+          ) : (
+            <input
+              className={styles.input}
+              type="text"
+              value={subTitle}
+              onChange={(e) => setSubTitle(e.target.value)}
+              onKeyDown={(event) => {
+                if (event.key === "Enter" || event.key === "Escape") {
+                  setToggleSubTitle(true);
+                  event.preventDefault();
+                  event.stopPropagation();
+                }
+              }}
+            />
+          )}
         </div>
         <div className={styles.seeAll}>
           <div className={styles.wrapBtn}>
