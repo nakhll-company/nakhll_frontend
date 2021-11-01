e34c76781b5e80a69c33dc64cb360299818cd8e4
sahar <s.shafiee_73@yahoo.com>
2021-11-01 20:31:02 +0330

    Fix(game): change game styles

--- pages/game/finish/index.jsx
+++ pages/game/finish/index.jsx
@@ -5,14 +5,10 @@ import Link from "next/link";
 import Image from 'next/image';
 function index(props) {
   let tl = new gsap.timeline();
-  let box1 = useRef(null);
   let box2 = useRef(null);
   useEffect(() => {
     // for Animation
-    tl.from(box1, {
-      opacity: 0,
-      duration: 20,
-    }).from(box2, {
+    tl.from(box2, {
       opacity: 0,
       scale: 0,
       ease: "back",
@@ -23,23 +19,11 @@ function index(props) {
   return (
     <>
       <div className={styles.wrap}>
-        <div ref={(el) => (box1 = el)} className={styles.onePage}>
-          <Image src="/image/game/last_1.jpg" layout="fill"
-            objectFit="cover"
-            quality={10} />
-          <span>
-            رطب نخل!<br />
-            بزرگ‌ترین تفاوت نخل با بازی مرکب اینه:<br />
-            اگر در بازی مرکب، تعداد شرکت‌کننده‌ها در گذر زمان کم و کم‌تر می‌شد و مردم به جون هم میفتادن تا به یه پول کلان برسن، بازار نخل، رویه‌ای کاملا متفاوت داره!
-            چون این‌جا لحظه به لحظه به تعداد شرکت‌کننده‌هاش اضافه می‌شه و در آخر همه با هم برنده می‌شن.
-            (این بازی دیگه پایان نداره...)
-          </span>
-        </div>
         <div ref={(el) => (box2 = el)} className={styles.twoPage}>
           <Image src="/image/game/last.jpg" layout="fill"
             objectFit="cover"
             quality={10} />
-          <Link href="/fp/store/create/">
+          <Link href="https://nakhll.com/accounts/get-phone/">
             <a>
               تیله اول رو بدست بیار<br />
               کلیک کنید
--- pages/game/four/index.jsx
+++ pages/game/four/index.jsx
@@ -11,7 +11,8 @@ function index(props) {
       <div className={styles.onePage}>
         <Image src="/image/game/04.jpg" layout="fill"
           objectFit="cover"
-          quality={10} />
+        // quality={10}
+        />
         <div id="58136310496">
           <Script id="one_script" src="https://www.aparat.com/embed/GbpMK?data[rnddiv]=58136310496&data[responsive]=yes&&recom=none" />
         </div>
