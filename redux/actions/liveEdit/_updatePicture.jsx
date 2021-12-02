7c28a6f673989e56111b6712394ea13178e44bb9
miillaadddddd <milad7212@gmail.com>
2021-12-02 09:54:58 +0330

    Refactor : call api picture

--- redux/actions/liveEdit/_updatePicture.jsx
+++ redux/actions/liveEdit/_updatePicture.jsx
@@ -1,7 +1,28 @@
+import { ApiRegister } from "../../../services/apiRegister/ApiRegister";
+
 export const _updatePicture = (img) => {
   return async (dispatch, getState) => {
     const id = getState().selectIdFormLanding;
-   
+
+    try {
+      let params = {};
+      let loadData = img;
+      let dataUrl = `/api/v1/profile/images/`;
+      let response = await ApiRegister().apiRequest(
+        loadData,
+        "POST",
+        dataUrl,
+        true,
+        params
+      );
+
+      if (response.status === 200) {
+      } else {
+        alert(response);
+      }
+    } catch (err) {
+      alert(err);
+    }
 
     let dataLanding = [...getState().allDataLanding];
     dataLanding.map((El, index) => {
