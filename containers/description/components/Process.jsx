1adf07075e345bbf55214eb0114f9bf514dd6a9a
miillaadddddd <milad7212@gmail.com>
2021-12-04 16:13:57 +0330

    Refactor(descriptionPage): finish  Process Component

--- containers/description/components/Process.jsx
+++ containers/description/components/Process.jsx
@@ -1,7 +1,25 @@
+import Image from "next/image";
+
 function Process() {
   return (
     <>
-      <div className="h-25">process</div>
+      <div className="hidden md:block">
+        <Image
+          src="/image/description/02.png"
+          layout="responsive"
+          width={4}
+          height={1.1}
+        />
+      </div>
+
+      <div className="md:hidden">
+        <Image
+          src="/image/description/01.png"
+          layout="responsive"
+          width={1}
+          height={1.8}
+        />
+      </div>
     </>
   );
 }
