444ee578b727c01078eb95f950ddb5d2cc7cf77e
miillaadddddd <milad7212@gmail.com>
2021-12-04 12:13:25 +0330

    Feat(descriptionPage):add icons

--- pages/description/index.jsx
+++ pages/description/index.jsx
@@ -1,13 +1,16 @@
 import "tailwindcss/tailwind.css";
 import Image from "next/image";
+import FeaturesBlocks from "../../containers/description/components/FeaturesBlocks";
 
 function index() {
   return (
     <div className="px-3 ">
       {/* Video */}
       <div className="bg-gray-700 w-full">
-        <Image src="" layout="responsive" width={500} height={500} />
+        {/* <Image src="" layout="responsive" width={500} height={500} /> */}
       </div>
+      {/* icons */}
+      <FeaturesBlocks />
     </div>
   );
 }
