df303c6e363686ecc25bbc0420932a374909d78e
miillaadddddd <milad7212@gmail.com>
2021-12-06 12:23:34 +0330

    Fix (cartPage):fix imports

--- api/cartAddress/addAddress.js
+++ api/cartAddress/addAddress.js
@@ -2,14 +2,16 @@ import { errorMessage } from "../../containers/utils/message";
 import { ApiRegister } from "../../services/apiRegister/ApiRegister";
 // get address of user
 export async function addAddress(data) {
-    let response = await ApiRegister().apiRequest(
-        data, "POST",
-        "/logistic/api/address/",
-        true, ""
-    );
-    if (response.status === 201) {
-        return true;
-    } else {
-        errorMessage("خطایی در دریافت داده ها پیش آمده است");
-    }
+  let response = await ApiRegister().apiRequest(
+    data,
+    "POST",
+    "/logistic/api/address/",
+    true,
+    ""
+  );
+  if (response.status === 201) {
+    return true;
+  } else {
+    errorMessage("خطایی در دریافت داده ها پیش آمده است");
+  }
 }
--- containers/cartAddress/deleteAddress.jsx
+++ containers/cartAddress/deleteAddress.jsx
@@ -1,25 +1,39 @@
 // methods
+import { deleteAddress } from "../../api/cartAddress/deleteAddress";
 import { getAddress } from "../../api/cartAddress/getAddress";
-import { deleteAddress } from './methods/deleteAddress';
+
 // styles
-import styles from '../../styles/pages/cart/deleteAddress.module.scss';
+import styles from "../../styles/pages/cart/deleteAddress.module.scss";
 /**
  * component delete modal
  */
 const DeleteAddress = ({ onClose, id, setAddress }) => {
-    return (
-        <div>
-            <h5 className={styles.text}>آیا از حذف آدرس خود مطمئن هستید؟</h5>
-            <div className={styles.wrapper_button}>
-                <button type="button" onClick={async () => {
-                    await deleteAddress(id);
-                    await getAddress(setAddress);
-                    await onClose();
-                }}>بله</button>
-                <button type="button" style={{ color: "red" }} onClick={() => { onClose() }}>خیر</button>
-            </div>
-        </div>
-    );
-}
+  return (
+    <div>
+      <h5 className={styles.text}>آیا از حذف آدرس خود مطمئن هستید؟</h5>
+      <div className={styles.wrapper_button}>
+        <button
+          type="button"
+          onClick={async () => {
+            await deleteAddress(id);
+            await getAddress(setAddress);
+            await onClose();
+          }}
+        >
+          بله
+        </button>
+        <button
+          type="button"
+          style={{ color: "red" }}
+          onClick={() => {
+            onClose();
+          }}
+        >
+          خیر
+        </button>
+      </div>
+    </div>
+  );
+};
 // export
 export default DeleteAddress;
