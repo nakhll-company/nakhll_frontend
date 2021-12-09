0b69977bed4186e81c1bca9c504943f9e162cd1e
miillaadddddd <milad7212@gmail.com>
2021-12-09 19:09:59 +0330

    fix(build) : fix bug biuld

--- containers/description/components/Questions.jsx
+++ containers/description/components/Questions.jsx
@@ -35,19 +35,6 @@ function Questions() {
           </div>
 
           {/* <!-- Card 2 --> */}
-<<<<<<< HEAD
-          <div className="transform transition cursor-pointer hover:-translate-y-2 ml-10 relative flex items-center px-6 py-4 bg-pink-600 text-white rounded mb-10 flex-col md:flex-row space-y-4 md:space-y-0">
-            {/* <!-- Dot Follwing the Left Vertical Line --> */}
-            <div className="w-5 h-5 bg-pink-600 absolute -left-10 transform -translate-x-2/4 rounded-full z-10 mt-2 md:mt-0"></div>
-
-            {/* <!-- Line that connecting the box with the vertical line --> */}
-            <div className="w-10 h-1 bg-pink-300 absolute -left-10 z-0"></div>
-
-            {/* <!-- Content that showing in the box --> */}
-            <div className="flex-auto">
-              <h1 className="text-xl font-bold">کارمزد نخل چند درصد است؟</h1>
-              <h3 className="mt-3 font-bold">
-=======
           <div
             style={{ backgroundColor: "rgba(219, 39, 119, 1)" }}
             className={styles.parent_card}
@@ -70,28 +57,12 @@ function Questions() {
                 کارمزد نخل چند درصد است؟
               </h1>
               <h3 className={styles.content_box_qu}>
->>>>>>> master
                 کارمزد نخل از هر تراکنش ۳ الی ۷ درصد است
               </h3>
             </div>
           </div>
 
           {/* <!-- Card 3 --> */}
-<<<<<<< HEAD
-          <div className="transform transition cursor-pointer hover:-translate-y-2 ml-10 relative flex items-center px-6 py-4 bg-green-600 text-white rounded mb-10 flex-col md:flex-row space-y-4 md:space-y-0">
-            {/* <!-- Dot Follwing the Left Vertical Line --> */}
-            <div className="w-5 h-5 bg-green-600 absolute -left-10 transform -translate-x-2/4 rounded-full z-10 mt-2 md:mt-0"></div>
-
-            {/* <!-- Line that connecting the box with the vertical line --> */}
-            <div className="w-10 h-1 bg-green-300 absolute -left-10 z-0"></div>
-
-            {/* <!-- Content that showing in the box --> */}
-            <div className="flex-auto">
-              <h1 className="text-xl font-bold">
-                تسویه حساب نخل با حجره‌داران چگونه است؟
-              </h1>
-              <h3 className="mt-3 font-bold">
-=======
           <div
             style={{
               backgroundColor: "  rgba(5, 150, 105, 1)",
@@ -116,7 +87,6 @@ function Questions() {
                 تسویه حساب نخل با حجره‌داران چگونه است؟
               </h1>
               <h3 className={styles.content_box_qu}>
->>>>>>> master
                 ‌پس از خرید از نخل و واریز پول توسط مشتری، سامانه شاپرک بعد از
                 ۲۴ ساعت پول واریزی مشتری را به حساب نخل واریز می‌کند و مراحل
                 پشتیبانی نخل شامل موارد ذیل است: ۱. پیگیری ارسال کالا توسط
@@ -131,21 +101,6 @@ function Questions() {
           </div>
 
           {/* <!-- Card 4 --> */}
-<<<<<<< HEAD
-          <div className="transform transition cursor-pointer hover:-translate-y-2 ml-10 relative flex items-center px-6 py-4 bg-purple-600 text-white rounded mb-10 flex-col md:flex-row space-y-4 md:space-y-0">
-            {/* <!-- Dot Follwing the Left Vertical Line --> */}
-            <div className="w-5 h-5 bg-purple-600 absolute -left-10 transform -translate-x-2/4 rounded-full z-10 mt-2 md:mt-0"></div>
-
-            {/* <!-- Line that connecting the box with the vertical line --> */}
-            <div className="w-10 h-1 bg-purple-300 absolute -left-10 z-0"></div>
-
-            {/* <!-- Content that showing in the box --> */}
-            <div className="flex-auto">
-              <h1 className="text-xl font-bold">
-                ارسال سفارش به چه طریقی انجام می‌شود؟
-              </h1>
-              <h3 className="mt-3 font-bold">
-=======
           <div
             style={{
               backgroundColor: " rgba(124, 58, 237, 1)",
@@ -170,7 +125,6 @@ function Questions() {
                 ارسال سفارش به چه طریقی انجام می‌شود؟
               </h1>
               <h3 className={styles.content_box_qu}>
->>>>>>> master
                 پس از اتمام فرآیند خرید مشتری، حجره‌دار سفارش را بسته‌بندی و از
                 طریق پست ارسال می‌کند. تمامی مراحل ارسال توسط پنل مدیریت
                 حجره‌دار اطلاع‌رسانی می‌گردد که توسط مشتری قابل مشاهده است.
