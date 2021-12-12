dea2636e40af7b1c17df03f08fcbde6332f1780b
miillaadddddd <milad7212@gmail.com>
2021-12-12 10:49:54 +0330

    Feature : Add next-seo

---
+++ next-seo.config.js
@@ -0,0 +1,15 @@
+export default {
+    title: "بازار اجتماعی نخل",
+
+    openGraph: {
+        type: "website",
+        locale: "en_IE",
+        url: "https://www.url.ie/",
+        site_name: "SiteName",
+    },
+    twitter: {
+        handle: "@handle",
+        site: "@site",
+        cardType: "summary_large_image",
+    },
+};
