6c9f8b64d3c35ce4d42eb8e677c111083c9f185a
miillaadddddd <milad7212@gmail.com>
2021-12-13 10:02:05 +0330

    Refactor: improve secret Button

--- containers/settings/components/logisticPage/index.jsx
+++ containers/settings/components/logisticPage/index.jsx
@@ -6,17 +6,16 @@ import SendBox from "./components/sendBox";
 import st from "./logisticPage.module.scss";
 
 function LogisticPage() {
-  const [isShow, setIsShow] = useState(false);
+  const [isShow, setIsShow] = useState(0);
   return (
     <>
-      <button onClick={() => setIsShow(!isShow)}>.</button>
-      {isShow && (
+      <button onClick={() => setIsShow(() => isShow + 1)}>.</button>
+      {isShow == 7 && (
         <div className={st.main}>
           <HeaderTitle title="تنظیمات لجستیک" />
 
           <Explain text="توضیحات به حجره دار" />
 
-          <SendBox title="پست معمولی" description="توضیحات سرویس" />
           <SendBox title="پست پیشتاز" description="توضیحات سرویس" />
           <SendBox title="پست سفارشی " description="توضیحات سرویس" />
           <SendBox title="ارسال رایگان" description="توضیحات سرویس" />
