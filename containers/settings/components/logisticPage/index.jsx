cd6aa1d43d693db8a2023eba9b99a004cc802808
miillaadddddd <milad7212@gmail.com>
2021-12-13 09:57:06 +0330

    Refctor(logisticPage): add Secret ‌Btn

--- containers/settings/components/logisticPage/index.jsx
+++ containers/settings/components/logisticPage/index.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Explain from "./components/explain";
 import HeaderTitle from "./components/headerTitle";
 import SendBox from "./components/sendBox";
@@ -5,19 +6,23 @@ import SendBox from "./components/sendBox";
 import st from "./logisticPage.module.scss";
 
 function LogisticPage() {
+  const [isShow, setIsShow] = useState(false);
   return (
     <>
-      <div className={st.main}>
-        <HeaderTitle title="تنظیمات لجستیک" />
+      <button onClick={() => setIsShow(!isShow)}>.</button>
+      {isShow && (
+        <div className={st.main}>
+          <HeaderTitle title="تنظیمات لجستیک" />
 
-        <Explain text="توضیحات به حجره دار" />
+          <Explain text="توضیحات به حجره دار" />
 
-        <SendBox title="پست معمولی" description="توضیحات سرویس" />
-        <SendBox title="پست پیشتاز" description="توضیحات سرویس" />
-        <SendBox title="پست سفارشی " description="توضیحات سرویس" />
-        <SendBox title="ارسال رایگان" description="توضیحات سرویس" />
-        <SendBox title="ارسال پس کرایه" description="توضیحات سرویس" />
-      </div>
+          <SendBox title="پست معمولی" description="توضیحات سرویس" />
+          <SendBox title="پست پیشتاز" description="توضیحات سرویس" />
+          <SendBox title="پست سفارشی " description="توضیحات سرویس" />
+          <SendBox title="ارسال رایگان" description="توضیحات سرویس" />
+          <SendBox title="ارسال پس کرایه" description="توضیحات سرویس" />
+        </div>
+      )}
     </>
   );
 }
