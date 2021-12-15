2ddd9f22d81cf48b517ff336fee75fbb337b7291
sahar <s.shafiee_73@yahoo.com>
2021-12-15 15:27:14 +0330

    Feat(product): add pagination in product list

--- redux/reducers/product/index.js
+++ redux/reducers/product/index.js
@@ -1,14 +1,14 @@
 import * as Types from '../../types/product';
 
 let initialize = {
-    productList: []
+    productList: {}
 }
 
 function reducer(state = initialize, actions) {
     switch (actions.type) {
         case Types.GET_PRODUCT:
             return {
-                productList: [...actions.payload]
+                productList: { ...actions.payload }
             }
         default:
             return state;
