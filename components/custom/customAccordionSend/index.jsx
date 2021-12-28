9c8aec196d99498f432f15f1b13cad2b901e01c2
miillaadddddd <milad7212@gmail.com>
2021-12-28 09:47:54 +0330

    Fix(SendPage): fix price

--- components/custom/customAccordionSend/index.jsx
+++ components/custom/customAccordionSend/index.jsx
@@ -117,7 +117,7 @@ export const CustomAccordionSend = ({
                 {logistic_price == 0 ? (
                   "رایگان"
                 ) : (
-                  <Number num={logistic_price} />
+                  <Number num={logistic_price / 10} />
                 )}
               </span>
             </div>
