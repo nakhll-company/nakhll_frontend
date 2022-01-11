b1ea358c56384b00edd9a48a4366582769f4e797
miillaadddddd <milad7212@gmail.com>
2022-01-11 16:56:54 +0330

    Refactor(shopCart) :add component

---
+++ components/ui/shopCart/index.jsx
@@ -0,0 +1,43 @@
+import Image from "next/image";
+import st from "./shopCart.module.scss";
+function ShopCart({ dataCart }) {
+  return (
+    <div className={st.a_box}>
+      <div className={st.img_container}>
+        <div className={st.img_inner}>
+          <div className={st.inner_skew}>
+            <img src={dataCart.products[0].Image_medium_url} />
+          </div>
+        </div>
+      </div>
+      <div className={st.icon_container}>
+        <Image
+          layout="fixed"
+          height={50}
+          width={50}
+          src="/image/mahsol.svg"
+          alt="camp"
+        />
+      </div>
+      {/* <div className=""></div> */}
+      {/* <div className={`${st.img_container} ${st.img_container_left}`}>
+        <div className={st.img_inner}>
+          <div className={st.inner_skew_left}>
+            <Image
+              layout="fill"
+              // height={250}
+              // width={200}
+              src={dataCart.products[1].Image_medium_url}
+            />
+          </div>
+        </div>
+      </div> */}
+      <div className={st.text_container}>
+        <h3>نوبت مامانه</h3>
+        <div>حجره {dataCart.Title}</div>
+      </div>
+    </div>
+  );
+}
+
+export default ShopCart;
