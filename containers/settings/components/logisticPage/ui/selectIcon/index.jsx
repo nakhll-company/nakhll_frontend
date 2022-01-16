b53ca95e107fb707f1925a7ff5bbcdadca8bd5b4
miillaadddddd <milad7212@gmail.com>
2022-01-16 10:01:26 +0330

    Feat(select icon): done ui

--- containers/settings/components/logisticPage/ui/selectIcon/index.jsx
+++ containers/settings/components/logisticPage/ui/selectIcon/index.jsx
@@ -1,10 +1,21 @@
 import { useForm } from "react-hook-form";
-
+import Image from "next/image";
 import InputUseForm from "../../../../../creat/component/inputUseForm";
 import BtnSetting from "../../components/btnSetting";
+import st from "./selectIcon.module.scss";
+
+const icons = [
+  { src: "/icons/settings/pishtaz.svg" },
+  { src: "/icons/settings/sefareshi.svg" },
+  { src: "/icons/settings/free.svg" },
+  { src: "/icons/settings/pasKeraieh.svg" },
+  { src: "/icons/settings/peik.svg" },
+  { src: "/icons/settings/plus.svg" },
+];
 
 function SelectIcon({ pageController }) {
   console.log(`Ren`, "SelectIcon");
+
   const {
     setValue,
     getValues,
@@ -25,6 +36,24 @@ function SelectIcon({ pageController }) {
           <input {...register("name")} />
         </InputUseForm>
       </form>
+
+      <div className={st.header}>
+        <span>لوگو روش ارسال</span>
+      </div>
+      {/* icones */}
+      <div className={st.warpperIcons}>
+        {icons.map((icon, index) => (
+          <div key={index} className={st.wrappIcon}>
+            <Image
+              src={icon.src}
+              layout="fixed"
+              height={50}
+              width={50}
+              alt="banner"
+            />
+          </div>
+        ))}
+      </div>
       <BtnSetting onClick={() => pageController()} title="ثبت" />
     </>
   );
