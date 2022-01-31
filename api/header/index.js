ce71edd00329fe3ea149ce35b070373a4fbd7e09
miillaadddddd <milad7212@gmail.com>
2022-01-31 09:50:29 +0330

    Fix (heaser) : fix call api

--- api/header/index.js
+++ api/header/index.js
@@ -1,47 +1,48 @@
 import { ApiReference } from "../../api/Api";
 import { ApiRegister } from "../../services/apiRegister/ApiRegister";
 
-
-export const _call_Category = async (setCategory) => {
-    try {
-        let response = await ApiRegister().apiRequest(
-            null,
-            "get",
-            `/api/v1/categories/?max_depth=2`,
-            false,
-            {}
-        );
-        if (response.status === 200) {
-            setCategory(response.data);
-        }
-    } catch (e) { }
+export const _call_Category = async () => {
+  try {
+    let response = await ApiRegister().apiRequest(
+      null,
+      "get",
+      `/api/v1/categories/?max_depth=2`,
+      false,
+      {}
+    );
+    if (response.status === 200) {
+      return response.data;
+    } else {
+      return [];
+    }
+  } catch (e) {}
 };
 
 // Get all shops
-export const _get_all_shops = async (shopsName, setShopsName) => {
-    if (shopsName.length == 0) {
-        let shops = await ApiRegister().apiRequest(
-            null,
-            "GET",
-            ApiReference.allShops,
-            false,
-            ""
-        );
+export const _get_all_shops = async () => {
+  let shops = await ApiRegister().apiRequest(
+    null,
+    "GET",
+    ApiReference.allShops,
+    false,
+    ""
+  );
 
-        if (shops.status === 200) {
-            setShopsName(shops.data);
-        }
-    }
+  if (shops.status === 200) {
+    return shops.data;
+  } else {
+    return [];
+  }
 };
 
-
 // Function for search
-export const _handel_search = (word, setInputSearch, setSearchShops, shopsName) => {
-    setInputSearch(word);
-    let copy_Array = [...shopsName];
-    let filterArray = [];
-    if (word != "") {
-        filterArray = copy_Array.filter((el) => el.title.includes(word));
-    }
-    setSearchShops(filterArray);
+export const _handel_search = (word, shopsName) => {
+  let copy_Array = shopsName ? [...shopsName] : [];
+  let filterArray = [];
+  if (word != "") {
+    filterArray = copy_Array.filter((el) => el.title.includes(word));
+  }
+  console.log("filterArray", filterArray);
+
+  return filterArray;
 };
