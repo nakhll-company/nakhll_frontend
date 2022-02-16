4669d54c51654858db75f91180fa92dfb94b977d
miillaadddddd <milad7212@gmail.com>
2022-02-16 09:50:46 +0330

    Fix (TwoValue component) : fix show component

--- containers/LandingPage/LinerTwoValue/index.jsx
+++ containers/LandingPage/LinerTwoValue/index.jsx
@@ -1,46 +1,41 @@
-// node libraries
-import React, { Fragment } from "react";
-import Image from "next/image";
-// style
+import React from "react";
+
 import styles from "./LinerTwoValue.module.scss";
 
 function LinerTwoValue({ dataLinerTwoValue }) {
   return (
-    <Fragment key={Math.random() + 7}>
+    <>
       {dataLinerTwoValue && dataLinerTwoValue.length >= 2 && (
         <>
           {/* for mobile */}
-          <div className={`container d-lg-none ${styles.mobile}`}>
+          {/* <div className={`container d-lg-none ${styles.mobile}`}>
             <div className="d-flex flex-row-reverse">
-              <Image
+              <img
                 src={dataLinerTwoValue[0].image}
                 loading="lazy"
                 alt={dataLinerTwoValue[0].title}
-                layout={"fill"}
               />
               <h4 className="ms-2">{dataLinerTwoValue[0].title}</h4>
             </div>
 
             <div className="d-flex flex-row-reverse">
-              <Image
+              <img
                 src={dataLinerTwoValue[1].image}
                 loading="lazy"
                 alt={dataLinerTwoValue[1].title}
-                layout={"fill"}
               />
               <h4>{dataLinerTwoValue[1].title}</h4>
             </div>
-          </div>
+          </div> */}
 
-          <div className="container d-none d-lg-block mt-2 mb-5">
+          <div className="container  d-block mt-2 mb-5">
             <aside className={styles.section}>
               <div className={styles.rightImg}>
-                <Image
-                  className={styles.image_on_desctop}
+                <img
+                  style={{ height: "auto" }}
                   src={dataLinerTwoValue[0].image}
                   loading="lazy"
                   alt={dataLinerTwoValue[0].title}
-                  layout={"fill"}
                 />
                 <span style={{ fontSize: "15px" }}>
                   {dataLinerTwoValue[0].title}
@@ -48,12 +43,11 @@ function LinerTwoValue({ dataLinerTwoValue }) {
               </div>
 
               <div className={styles.leftImg}>
-                <Image
-                  className={styles.image_on_desctop}
+                <img
+                  style={{ height: "auto" }}
                   src={dataLinerTwoValue[1].image}
                   loading="lazy"
                   alt={dataLinerTwoValue[1].title}
-                  layout={"fill"}
                 />
                 <span style={{ fontSize: "15px" }}>
                   {dataLinerTwoValue[1].title}
@@ -61,10 +55,9 @@ function LinerTwoValue({ dataLinerTwoValue }) {
               </div>
             </aside>
           </div>
-          {/* for Desktop */}
         </>
       )}
-    </Fragment>
+    </>
   );
 }
 
