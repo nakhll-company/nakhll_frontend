8a6c326eb36b71721c87f2f3432f8d74a5bb85d3
miillaadddddd <milad7212@gmail.com>
2022-03-09 12:04:13 +0330

    Add Updat site

--- next.config.js
+++ next.config.js
@@ -1,10 +1,43 @@
 module.exports = {
+    async redirects() {
+        return [{
+                source: "/",
+                destination: "/update",
+                permanent: false,
+            },
+            {
+                source: "/404",
+                destination: "/update",
+                permanent: false,
+            },
+            {
+                source: "/fp/:slug*",
+                destination: "/update",
+                permanent: false,
+            },
+            {
+                source: "/search/:slug*",
+                destination: "/update",
+                permanent: false,
+            },
+            {
+                source: "/login/:slug*",
+                destination: "/update",
+                permanent: false,
+            },
+            {
+                source: "/shop/:slug*",
+                destination: "/update",
+                permanent: false,
+            },
+        ];
+    },
     i18n: {
         locales: ["fa"],
         defaultLocale: "fa",
     },
     env: {
-        BASE_URL: "https://nakhll.com/",
+        BASE_URL: "http://localhost:8000/",
     },
     images: {
         domains: ["nakhll.com", "3007", "localhost"],
