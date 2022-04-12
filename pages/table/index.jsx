a8fa8aa83f6d89f5160b18c5a53ab1bd0496380c
miillaadddddd <milad7212@gmail.com>
2022-04-12 13:47:52 +0430

    Feat : Add script for test

---
+++ pages/table/index.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import EmptyLayout from "../../components/layout/EmptyLayout";
+import Script from "next/script";
+function Table() {
+  return (
+    <div style={{padding:"20px"}}>
+      <Script
+        id="tableEnfo"
+        strategy="beforeInteractive"
+        src={`//js.hsforms.net/forms/shell.js`}
+      />
+      <Script id="ExtraTable" strategy="afterInteractive">
+        {`hbspt.forms.create({
+ 	region: "na1",
+ 	portalId: "21759198",
+ 	formId: "84c62938-a700-413f-ad36-d74274f4a7c6"
+ });`}
+      </Script>
+      
+    </div>
+    
+  );
+}
+
+export default Table;
+Table.Layout = EmptyLayout;
