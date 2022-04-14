704c9d77d96b315c035349959726332f369ea77e
miillaadddddd <milad7212@gmail.com>
2022-04-14 11:23:25 +0430

    change style form hubSpot

--- pages/table/index.jsx
+++ pages/table/index.jsx
@@ -1,24 +1,23 @@
 import React from "react";
 import EmptyLayout from "../../components/layout/EmptyLayout";
 import Script from "next/script";
+
 function Table() {
   return (
-    <div style={{padding:"20px"}}>
+    <div>
       <Script
         id="tableEnfo"
         strategy="beforeInteractive"
         src={`//js.hsforms.net/forms/shell.js`}
       />
-      <Script id="ExtraTable" strategy="afterInteractive">
+      <Script id="ExtraTable" strategy="lazyOnload">
         {`hbspt.forms.create({
  	region: "na1",
  	portalId: "21759198",
  	formId: "84c62938-a700-413f-ad36-d74274f4a7c6"
  });`}
       </Script>
-      
     </div>
-    
   );
 }
 
