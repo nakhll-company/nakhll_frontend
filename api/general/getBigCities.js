67d1dd966949e3af25d1fa85a0412a307dafda61
miillaadddddd <milad7212@gmail.com>
2022-04-24 13:55:21 +0430

    Fix error in select Cities

--- api/general/getBigCities.js
+++ api/general/getBigCities.js
@@ -2,7 +2,7 @@ import { http } from "../../services/callApi/api";
 // big city
 export const getBigCities = async(id) => {
     try {
-        let response = http.get(`/api/v1/get-big-cities/?state_id=${id}`);
+        let response = await http.get(`/api/v1/get-big-cities/?state_id=${id}`);
         if (response.status === 200) {
             return response.data;
         }
