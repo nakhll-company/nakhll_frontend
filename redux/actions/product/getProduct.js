3854920292fdcd4cda42797e54b49684744f859b
sahar <s.shafiee_73@yahoo.com>
2022-05-08 11:48:10 +0430

    Fix(product): send params in product api

--- redux/actions/product/getProduct.js
+++ redux/actions/product/getProduct.js
@@ -13,36 +13,37 @@ export const getProduct =
         search,
         page
     ) =>
-    async(dispatch) => {
-        // try
-        try {
-            if (activeHojreh.length > 0) {
-                const getProduct = async() => {
-                    let params = {
-                        product_status,
-                        price_from,
-                        price_to,
-                        inventory_from,
-                        inventory_to,
-                        order_by,
-                        search,
-                        page
+        async (dispatch) => {
+            console.log("page", page);
+            // try
+            try {
+                if (activeHojreh.length > 0) {
+                    const getProduct = async () => {
+                        let params = {
+                            product_status,
+                            price_from,
+                            price_to,
+                            inventory_from,
+                            inventory_to,
+                            order_by,
+                            search,
+                            page
+                        };
+
+                        let dataUrl = `/api/v1/shop/${activeHojreh}/products/`;
+                        let response = await authhttp.get(dataUrl, { params })
+                        return response;
                     };
 
-                    let dataUrl = `/api/v1/shop/${activeHojreh}/products/`;
-                    let response = await authhttp.get(dataUrl, params)
-                    return response;
-                };
+                    let response = await getProduct();
 
-                let response = await getProduct();
-
-                if (response.status === 200) {
-                    // dispatch
-                    dispatch({
-                        type: Types.GET_PRODUCT,
-                        payload: response.data,
-                    });
+                    if (response.status === 200) {
+                        // dispatch
+                        dispatch({
+                            type: Types.GET_PRODUCT,
+                            payload: response.data,
+                        });
+                    }
                 }
-            }
-        } catch (error) {}
-    };
+            } catch (error) { }
+        };
