17a8b1ee29a2b8d290527ae0a76ac37536a8127e
miillaadddddd <milad7212@gmail.com>
2022-05-08 11:58:14 +0430

    Feat : search input

--- containers/nakhlPage/InputLanding.jsx
+++ containers/nakhlPage/InputLanding.jsx
@@ -1,22 +1,40 @@
 import { FaSearch } from "react-icons/fa";
 import Link from "next/link";
-import React from "react";
 import s from "./InputLanding.module.scss";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 function InputLanding() {
+  const router = useRouter();
+  const [inputSearch, setInputSearch] = useState("");
+  useEffect(() => {
+    console.log("inputSearch :>> ", inputSearch);
+  }, [inputSearch]);
+
   return (
-    <div className={s.inputContainer}>
-      <input
-        className={s.input}
-        placeholder="جست وجو در بیش از هزار فروشگاه..."
-      />
-      <Link href={`/search`}>
-        <a>
-          <div className={s.iconContainer}>
-            <FaSearch size="20px" />
-          </div>
-        </a>
-      </Link>
-    </div>
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+        router.push(`/search?q=${inputSearch}`);
+      }}
+    >
+      <div className={s.inputContainer}>
+        <input
+          className={s.input}
+          value={inputSearch}
+          onChange={(el) => setInputSearch(el.target.value)}
+          placeholder="جست وجو در بیش از هزار فروشگاه..."
+        />
+        {inputSearch !== "" && (
+          <Link href={`/search?q=${inputSearch}`}>
+            <a>
+              <div className={s.iconContainer}>
+                <FaSearch size="20px" />
+              </div>
+            </a>
+          </Link>
+        )}
+      </div>
+    </form>
   );
 }
 
