9619d47602cf357d37761cbb4361c6c7f2f6dbf7
miillaadddddd <milad7212@gmail.com>
2022-05-15 13:46:04 +0430

    Feature : #21 Add box for search in landing

--- containers/nakhlPage/InputLanding.jsx
+++ containers/nakhlPage/InputLanding.jsx
@@ -4,35 +4,55 @@ import Link from "next/link";
 import { FaSearch } from "react-icons/fa";
 
 import s from "./InputLanding.module.scss";
+import { _get_all_shops, _handel_search } from "../../api/header";
+import BoxSearch from "../../components/shopLayout/header/boxSearch";
 
 function InputLanding() {
   const router = useRouter();
   const [inputSearch, setInputSearch] = useState("");
+  const [shopsName, setShopsName] = useState([]);
+  const [searchShops, setSearchShops] = useState([]);
 
   return (
-    <form
-      onSubmit={(event) => {
-        event.preventDefault();
-        router.push(`/search?q=${inputSearch}`);
-      }}
-    >
-      <div className={s.inputContainer}>
-        <input
-          className={s.input}
-          value={inputSearch}
-          onChange={(el) => setInputSearch(el.target.value)}
-          placeholder="جست وجو در بیش از هزار فروشگاه..."
-        />
-
-        <Link href={inputSearch !== "" ? `/search?q=${inputSearch}` : ""}>
-          <a>
-            <div className={s.iconContainer}>
-              <FaSearch size="20px" />
-            </div>
-          </a>
-        </Link>
+    <div style={{position:'relative'}}  >
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          router.push(`/search?q=${inputSearch}`);
+        }}
+      >
+        <div className={s.inputContainer}>
+          <input
+            className={s.input}
+            value={inputSearch}
+            onClick={async () => {
+              if (shopsName.length == 0) {
+                let getShopsName = await _get_all_shops(shopsName);
+                setShopsName(getShopsName);
+              }
+            }}
+            onChange={(el) => {
+              setInputSearch(el.target.value);
+              let searchedShop = _handel_search(el.target.value, shopsName);
+              setSearchShops(searchedShop);
+            }}
+            placeholder="جست وجو در بیش از هزار فروشگاه..."
+          />
+          <Link href={inputSearch !== "" ? `/search?q=${inputSearch}` : ""}>
+            <a>
+              <div className={s.iconContainer}>
+                <FaSearch size="20px" />
+              </div>
+            </a>
+          </Link>
+        </div>
+      </form>
+      <div className={s.wrapBox} >
+      {searchShops.length > 0 && (
+        <BoxSearch list={searchShops} word={inputSearch} />
+      )}
       </div>
-    </form>
+    </div >
   );
 }
 
