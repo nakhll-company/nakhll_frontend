171f9f646a8a10f465ff87750e511b04404b78fb
miillaadddddd <milad7212@gmail.com>
2022-05-25 11:12:49 +0430

    Fix : #22  dont Updat pictures's shop

--- pages/fp/index.jsx
+++ pages/fp/index.jsx
@@ -1,23 +1,9 @@
 import React from "react";
-// node libraries
-import Head from "next/head";
 // components
 import Layout from "../../components/layout/Layout";
 import Dashboard from "../../containers/dashboard";
 
 export default function DashboardMain() {
-  return (
-    <>
-      <Head>
-        <link
-          rel="stylesheet"
-          href="https://use.fontawesome.com/releases/v5.8.2/css/all.css"
-        />
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <meta name="robots" content="noindex, nofollow" />
-      </Head>
-      <Dashboard />
-    </>
-  );
+  return (<Dashboard />);
 }
 DashboardMain.Layout = Layout;
