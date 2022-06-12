30661690c32e92847bb78e0e170983292b2b144a
miillaadddddd <milad7212@gmail.com>
2022-06-12 12:57:02 +0430

    Fix : #139

--- redux/actions/cart/_reduceProduct.jsx
+++ redux/actions/cart/_reduceProduct.jsx
@@ -1,7 +1,8 @@
-import { successMessage } from "../../../utils/toastifyMessage";
+import { errorMessage, successMessage } from "../../../utils/toastifyMessage";
 import { authhttp } from "../../../services/callApi/api";
 
-export const _reduceProduct = (productId) => {
+export const _reduceProduct = (productId, count) => {
+
   return async (dispatch) => {
     const response = await authhttp.delete(
       `/api/v1/cart/items/${productId}/reduce/`
@@ -9,7 +10,16 @@ export const _reduceProduct = (productId) => {
 
     await dispatch({ type: "َREDUCE_PRODUCT", payload: response.data });
 
-    // if (response.status === 200) {
-    successMessage("تعداد محصول با موفقیت کاهش پیدا کرد");
+    if (response.status === 200) {
+
+      if (count == 1) {
+        errorMessage('محصول از سبد خرید حذف شد')
+      } else {
+
+        successMessage("تعداد محصول با موفقیت کاهش پیدا کرد");
+      }
+
+    }
+
   };
 };
