70a84c04d3d7ef5d66aebd76c45c55db977940a7
sahar <s.shafiee_73@yahoo.com>
2022-06-14 10:50:40 +0430

    Correct api big cities

--- api/general/getBigCities.js
+++ api/general/getBigCities.js
@@ -3,7 +3,7 @@ import {
 } from "../../services/callApi/api";
 // big city
 export const getBigCities = async (id) => {
-    const response = await http.get(`/kkkkk/lfklgkfdg/?state_id=${id}`);
+    const response = await http.get(`/api/v1/get-big-cities/?state_id=${id}`);
     if (response.status === 200) {
         return response.data;
     } else {
