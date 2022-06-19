31df7a89e86a91eca6cafc05ce832d22624ec922
miillaadddddd <milad7212@gmail.com>
2022-06-19 11:06:59 +0430

    step one coin:add btn in profilePage

--- components/layout/ProfileLayout.jsx
+++ components/layout/ProfileLayout.jsx
@@ -3,18 +3,26 @@ import Image from "next/image";
 import Link from "next/link";
 import { useSelector } from "react-redux";
 import React, { useEffect, useState } from "react";
+import { RiUserSettingsFill } from "react-icons/ri";
+import { BsShopWindow } from "react-icons/bs";
 import { getUserData } from "../../containers/profile/methods/getUserData";
+import { MdOutlineFollowTheSigns, MdOutlineFavorite } from "react-icons/md";
+import { GiTwoCoins } from "react-icons/gi";
 import styles from "./profile.module.scss";
 import useViewport from "../viewPort";
 import { useRouter } from "next/router";
 import Header from "../shopLayout/header/header";
 import Footer from "../shopLayout/footer";
+
+// Main Component
 function ProfileLayout({ children }) {
   const breakpoint = 767;
   const router = useRouter();
+
   const { width } = useViewport();
   const [dataProfile, setDataProfile] = useState({});
   const userData = useSelector((state) => state.User.userInfo);
+
   useEffect(() => {
     async function fetchData() {
       const ans = await getUserData();
@@ -22,15 +30,36 @@ function ProfileLayout({ children }) {
     }
     fetchData();
   }, []);
+  // mini component list
+  function Li({ href, title, children }) {
+    let styleActive = {};
+    if (href == router.pathname) {
+      console.log("href :>> ", href);
+      console.log("router.pathname :>> ", router.pathname);
+      styleActive = {
+        backgroundColor: " #A06912",
+        boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.25)",
+        borderRadius: "8px",
+        color: "#FCFCFC",
+        fontSize: "16px",
+        padding: "16px",
+        fontWeight: "bolder",
+      };
+    }
+    return (
+      <li className="mb-3" style={styleActive}>
+        <Link href={href} scroll={false}>
+          <a className="d-flex align-items-center">
+            {children}
+            {title}
+          </a>
+        </Link>
+      </li>
+    );
+  }
   return (
     <>
       <Head>
-        <link
-          rel="stylesheet"
-          href="https://use.fontawesome.com/releases/v5.15.3/css/all.css"
-          integrity="sha384-SZXxX4whJ79/gErwcOYf+zWLeJdY/qpuqC4cAa9rOGUstPomtqpuNWT9wdPEn2fk"
-          crossOrigin="anonymous"
-        />
         <meta name="robots" content="noindex, nofollow" />
       </Head>
       <Header />
@@ -58,48 +87,38 @@ function ProfileLayout({ children }) {
                     `${dataProfile.FK_User.first_name} ${dataProfile.FK_User.last_name}`}
                 </h6>
               </li>
-              <hr />
-              <li
-                className="d-flex align-items-center mb-3"
-                onClick={async (event) => {
-                  router.push("/profile/");
-                }}
+
+              <div className={styles.wavy}></div>
+
+              <Li title="ویرایش پروفایل" href="/profile">
+                <RiUserSettingsFill size={25} style={{ marginLeft: "10px" }} />
+              </Li>
+
+              <Li title="پیگیری سفارش" href="/profile/orderdetail">
+                <MdOutlineFollowTheSigns
+                  size={25}
+                  style={{ marginLeft: "10px" }}
+                />
+              </Li>
+
+              <Li title="لیست علاقمندی ها" href="/profile/favList">
+                <MdOutlineFavorite size={25} style={{ marginLeft: "10px" }} />
+              </Li>
+
+              <Li title="انبار سکه" href="/profile/coins">
+                <GiTwoCoins size={25} style={{ marginLeft: "10px" }} />
+              </Li>
+
+              <Li
+                title="مدیریت حجره"
+                href={
+                  userData && userData.shops && userData.shops.length > 0
+                    ? "/fp"
+                    : "/fp/store/create"
+                }
               >
-                <i className="fas fa-user-cog ms-2"></i>
-                ویرایش پروفایل
-              </li>
-              <li className="mb-3">
-                <Link
-                  href={
-                    userData && userData.shops && userData.shops.length > 0
-                      ? "/fp"
-                      : "/fp/store/create"
-                  }
-                >
-                  <a className="d-flex align-items-center">
-                    <i className="fas fa-store ms-2"></i>
-                    مدیریت حجره
-                  </a>
-                </Link>
-              </li>
-              <li
-                className="d-flex align-items-center mb-3"
-                onClick={(event) => {
-                  router.push("/profile/orderdetail");
-                }}
-              >
-                <i className="fas fa-box-open ms-2"></i>
-                پیگیری سفارشات
-              </li>
-              <li
-                className="d-flex align-items-center mb-3"
-                onClick={(event) => {
-                  router.push("/profile/favList");
-                }}
-              >
-                <i className="far fa-bookmark ms-2"></i>
-                لیست علاقمندی ها
-              </li>
+                <BsShopWindow size={25} style={{ marginLeft: "10px" }} />
+              </Li>
             </ul>
           </div>
           {width < breakpoint && (
@@ -108,18 +127,7 @@ function ProfileLayout({ children }) {
                 <hr />
                 <li
                   className="d-flex flex-column align-items-center"
-                  onClick={(event) => {
-                    activeLink(event);
-                    setProfilePages(() => {
-                      return {
-                        ordersPage: false,
-                        favoritesList: false,
-                        shoppingExperiences: false,
-                        editProfile: true,
-                        orderDetail: false,
-                      };
-                    });
-                  }}
+                  onClick={(event) => {}}
                 >
                   <i
                     className="fas fa-user-cog"
@@ -152,18 +160,7 @@ function ProfileLayout({ children }) {
                 </li>
                 <li
                   className="d-flex flex-column align-items-center"
-                  onClick={(event) => {
-                    activeLink(event);
-                    setProfilePages(() => {
-                      return {
-                        editProfile: false,
-                        favoritesList: false,
-                        shoppingExperiences: false,
-                        ordersPage: true,
-                        orderDetail: false,
-                      };
-                    });
-                  }}
+                  onClick={(event) => {}}
                 >
                   <i
                     className="fas fa-box-open"
@@ -174,18 +171,7 @@ function ProfileLayout({ children }) {
                 </li>
                 <li
                   className="d-flex flex-column align-items-center "
-                  onClick={(event) => {
-                    activeLink(event);
-                    setProfilePages(() => {
-                      return {
-                        editProfile: false,
-                        ordersPage: false,
-                        shoppingExperiences: false,
-                        favoritesList: true,
-                        orderDetail: false,
-                      };
-                    });
-                  }}
+                  onClick={(event) => {}}
                 >
                   <i
                     className="far fa-bookmark"
---
+++ pages/profile/coins/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ProfileLayout from "../../../components/layout/ProfileLayout";
+
+function CoinsPage() {
+  return <div>CoinsPage</div>;
+}
+
+export default CoinsPage;
+CoinsPage.Layout = ProfileLayout;
