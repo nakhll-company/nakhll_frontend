0383ed77bcd792ebe6d6d900467d992e603fd026
miillaadddddd <milad7212@gmail.com>
2022-06-21 13:25:45 +0430

    coin part2

--- components/layout/MenuMobile.jsx
+++ components/layout/MenuMobile.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import styles from "../../styles/components/layout/MenuMobile.module.scss";
+import styles from "./MenuMobile.module.scss";
 
 function MenuMobile({ activeOptions }) {
   const router = useRouter();
