c276cd5c459b6c6a25a8f88861e6504c8407f4c1
miillaadddddd <milad7212@gmail.com>
2022-07-11 14:47:11 +0430

    Add tailwindcss

---
+++ pages/test.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function test() {
+  return (
+    <div className="bg-blue-400 rounded-xl flex justify-center items-center text-white font-black text-xl">
+      {" "}
+      N_a_k_h_l_l{" "}
+    </div>
+  );
+}
+
+export default test;
